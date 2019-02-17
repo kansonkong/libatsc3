6fbaf78a78e462d5bc9ece7fb69194fcab9eb65a
jjustman <jjustman@ngbp.org>
2019-02-17 08:31:40 -0800

    recon for mpu is now working

--- src/bento4/MPUtoISOBMFFProcessor.h
+++ src/bento4/MPUtoISOBMFFProcessor.h
@@ -11,7 +11,7 @@
 #define BENTO4_MPUTOISOBMFFPROCESSOR_H_
 
 
-AP4_MemoryByteStream* mpuToISOBMFFProcessBoxes(uint8_t* full_mpu_payload, uint32_t full_mpu_payload_size);
+AP4_DataBuffer* mpuToISOBMFFProcessBoxes(uint8_t* full_mpu_payload, uint32_t full_mpu_payload_size, int mdat_size);
 
 
 
