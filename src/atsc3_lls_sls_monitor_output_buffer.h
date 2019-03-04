3a5dfb2014381e7aadcd8c103d9a6f9a62b5275b
jjustman <jjustman@ngbp.org>
2019-03-03 16:29:40 -0800

    checkpoint with fragment refactoring

--- src/atsc3_lls_sls_monitor_output_buffer.h
+++ src/atsc3_lls_sls_monitor_output_buffer.h
@@ -11,6 +11,7 @@
 
 #include <stdbool.h>
 #include "atsc3_utils.h"
+#include "atsc3_player_ffplay.h"
 
 //4k gets pretty big pretty quick
 #define _LLS_SLS_MONITOR_OUTPUT_MAX_RECON_BUFFER 16384000
@@ -19,11 +20,20 @@
 typedef struct lls_sls_monitor_buffer {
     bool has_written_init_box;
     bool should_flush_output_buffer;
+    
     uint8_t* audio_output_buffer;
     uint32_t audio_output_buffer_pos;
     uint8_t* video_output_buffer;
     uint32_t video_output_buffer_pos;
 } lls_sls_monitor_output_buffer_t;
+
+
+typedef struct lls_sls_monitor_buffer_mode {
+    bool file_dump_enabled;
+    bool ffplay_output_enabled;
+    pipe_ffplay_buffer_t* pipe_ffplay_buffer;
+
+} lls_sls_monitor_output_buffer_mode_t;
 
 
 #endif /* atsc3_lls_sls_monitor_output_buffer_h */
