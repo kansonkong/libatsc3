5f557251b56068e03b767d58dd2fd835b6ea8dd7
jj <jjustman@ngbp.org>
2019-03-10 15:57:46 -0700

    updating hold count

--- src/atsc3_player_ffplay.h
+++ src/atsc3_player_ffplay.h
@@ -32,7 +32,7 @@ extern "C" {
 extern int _PLAYER_FFPLAY_DEBUG_ENABLED;
 extern int _PLAYER_FFPLAY_TRACE_ENABLED;
 
-#define __PLAYER_INITIAL_BUFFER_SEGMENT_COUNT 1  //wait for at least 1 signals
+#define __PLAYER_INITIAL_BUFFER_SEGMENT_COUNT 2  //wait for at least 1 signals
 #define __PLAYER_INITIAL_BUFFER_TARGET 512000  //and 1 MB of payload before starting to stream to ffplay
 #define __PLAYER_FFPLAY_BUFFER_WARNING_SIZE 128000
 #define __PLAYER_FFPLAY_PIPE_INTERNAL_BUFFER_SIZE 8192000
