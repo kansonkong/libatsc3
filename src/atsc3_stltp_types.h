3ac8bd7ee1daced2f4eee8858b13de96909bf713
jjustman <jjustman@ngbp.org>
2019-03-16 13:15:39 -0700

    stltp parsing and markers for baseband packets

--- src/atsc3_stltp_types.h
+++ src/atsc3_stltp_types.h
@@ -9,6 +9,7 @@
 #define ATSC3_STLTP_TYPES_H_
 
 #include <stdint.h>
+#include <stdbool.h>
 #include "atsc3_listener_udp.h"
 
 #define ATSC3_STLTP_PAYLOAD_TYPE_TUNNEL 			0x61
@@ -26,21 +27,33 @@ typedef struct atsc3_rtp_fixed_header {
 	uint32_t packet_offset;
 } atsc3_rtp_fixed_header_t;
 
-typedef struct atsc3_stltp_tunnel_packet {
-	atsc3_rtp_fixed_header_t* atsc3_rtp_fixed_header;
-	uint8_t* first_ip_header;
-	uint32_t first_ip_header_length;
-	udp_packet_t* udp_packet;
-
-} atsc3_stltp_tunnel_packet_t;
-
 
 typedef struct atsc3_stltp_baseband_packet {
 	atsc3_rtp_fixed_header_t* atsc3_rtp_fixed_header;
 	uint8_t* baseband_packet;
+	uint32_t baseband_packet_offset;
 	uint32_t baseband_packet_length;
 	udp_packet_t* udp_packet;
+	uint32_t fragment_count;
 
 } atsc3_stltp_baseband_packet_t;
+
+
+typedef struct atsc3_stltp_tunnel_packet {
+	atsc3_rtp_fixed_header_t* atsc3_rtp_fixed_header_tunnel;
+	uint8_t* first_ip_header;
+	uint32_t first_ip_header_length;
+
+	udp_packet_t* udp_packet;
+	uint32_t fragment_count;
+
+	atsc3_rtp_fixed_header_t* atsc3_rtp_fixed_header_payload;
+
+	atsc3_stltp_baseband_packet_t* atsc3_stltp_baseband_packet;
+	bool is_truncated_from_marker;
+	bool is_complete;
+
+} atsc3_stltp_tunnel_packet_t;
+
 
 #endif /* ATSC3_STLTP_TYPES_H_ */
