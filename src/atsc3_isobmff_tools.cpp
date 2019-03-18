fc788fe5219198d2e731c91c82c3fe110ccecac5
Jason Justman <jjustman@ngbp.org>
2019-03-18 06:14:12 -0700

    fixing video presentation time

--- src/atsc3_isobmff_tools.cpp
+++ src/atsc3_isobmff_tools.cpp
@@ -472,7 +472,7 @@ lls_sls_monitor_output_buffer_t* atsc3_isobmff_build_mpu_metadata_ftyp_moof_mdat
 										lls_sls_monitor_output_buffer->video_output_buffer_isobmff.mpu_presentation_time = mmt_signaling_message_mpu_tuple->mpu_presentation_time;
 										lls_sls_monitor_output_buffer->video_output_buffer_isobmff.mpu_presentation_time_set = true;
 
-										compute_ntp64_to_seconds_microseconds(lls_sls_monitor_output_buffer->video_output_buffer_isobmff.mpu_presentation_time, &lls_sls_monitor_output_buffer->audio_output_buffer_isobmff.mpu_presentation_time_s, &lls_sls_monitor_output_buffer->audio_output_buffer_isobmff.mpu_presentation_time_ms);
+										compute_ntp64_to_seconds_microseconds(lls_sls_monitor_output_buffer->video_output_buffer_isobmff.mpu_presentation_time, &lls_sls_monitor_output_buffer->video_output_buffer_isobmff.mpu_presentation_time_s, &lls_sls_monitor_output_buffer->video_output_buffer_isobmff.mpu_presentation_time_ms);
 
 										__ISOBMFF_TOOLS_WARN("setting video packet_id: %u, mpu_sequence_number: %u to mpu_presentation_time: %llu, seconds: %u, ms: %u",
 												mp_table_asset_row->mmt_general_location_info.packet_id, mpu_sequence_number_video,
