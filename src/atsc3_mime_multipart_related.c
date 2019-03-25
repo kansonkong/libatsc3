7014e0f6ea8f9ef4390043503602af7ea97a7041
jjustman <jjustman@ngbp.org>
2019-03-25 07:27:37 +0100

    multipart parsing

---
+++ src/atsc3_mime_multipart_related.c
@@ -0,0 +1,10 @@
+/*
+ * atsc3_mime_multipart_related.c
+ *
+ *  Created on: Mar 25, 2019
+ *      Author: jjustman
+ */
+
+#include "atsc3_mime_multipart_related.h"
+
+ATSC3_VECTOR_BUILDER_METHODS_IMPLEMENTATION(atsc3_mime_multipart_related_instance, atsc3_mime_multipart_related_payload)
