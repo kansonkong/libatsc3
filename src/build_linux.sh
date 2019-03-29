fbfa07fbf067b7f56fcf205033a83c1f75bc9dae
Jon Ribeiro <jonathas.ribeiro@mautilus.com>
2019-03-29 07:53:01 +0100

    Added locale linking to docker image

--- src/build_linux.sh
+++ src/build_linux.sh
@@ -4,7 +4,7 @@
 sudo ln -s /usr/include/locale.h /usr/include/xlocale.h
 
 # Compile libmicrohttpd
-cd ../libmicrohttpd && rm -Rf libmicrohttpd-0.9.63 && tar -xvzf libmicrohttpd-latest.tar.gz && cd libmicrohttpd-0.9.63 && ./configure --prefix=`pwd`/build && make install
+cd ../libmicrohttpd && tar -xvzf libmicrohttpd-latest.tar.gz && cd libmicrohttpd-0.9.63 && ./configure --prefix=`pwd`/build && make install
 
 # Compile Bento4
 cd ../../bento && mv lib lib-tmp && mkdir lib && mv lib-tmp/place_libap4.a_bento_file_here lib && mv lib-tmp/.gitignore lib && rm -Rf lib-tmp && cd lib && git clone https://github.com/axiomatic-systems/Bento4.git && cd Bento4 && mkdir cmakebuild && cd cmakebuild && cmake -DCMAKE_BUILD_TYPE=Debug .. && make && mv * ../..
