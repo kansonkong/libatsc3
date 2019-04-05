3a5620e410dda14a81ea03ad8f1cb0e471115e03
Jason Justman <jjustman@ngbp.org>
2019-04-05 06:44:21 -0700

    first pass at updating tsi/toi from mbms

--- src/atsc3_lls_alc_utils.h
+++ src/atsc3_lls_alc_utils.h
@@ -23,13 +23,6 @@
 #include "atsc3_lls_sls_parser.h"
 
 
-#define _LLS_PRINTLN(...) printf(__VA_ARGS__);printf("%s%s","\r","\n")
-#define _LLS_PRINTF(...)  printf(__VA_ARGS__);
-
-#define __LLSU_TRACE(...)
-//#define __LLSU_TRACE(...) _LLS_PRINTLN(__VA_ARGS__);
-
-
 #if defined (__cplusplus)
 extern "C" {
 #endif
@@ -48,6 +41,8 @@ lls_sls_alc_session_t* lls_slt_alc_session_find(lls_sls_alc_session_vector_t* ll
 lls_sls_alc_session_t* lls_slt_alc_session_find_from_udp_packet(lls_slt_monitor_t* lls_slt_monitor, uint32_t src_ip_addr, uint32_t dst_ip_addr, uint16_t dst_port);
 lls_sls_alc_session_t* lls_slt_alc_session_find_from_service_id(lls_slt_monitor_t* lls_slt_monitor, uint16_t service_id);
 
+void lls_sls_alc_update_tsi_toi_from_route_s_tsid(lls_sls_alc_monitor_t* lls_sls_alc_monitor, atsc3_route_s_tsid_t* atsc3_route_s_tsid);
+
 void lls_slt_alc_session_remove(lls_sls_alc_session_vector_t* lls_slt_alc_session, lls_service_t* lls_service);
 
 
@@ -60,6 +55,19 @@ void lls_sls_alc_session_free(lls_sls_alc_session_t** lls_session_ptr);
 #if defined (__cplusplus)
 }
 #endif
+
+
+
+#define _ATSC3_LLS_ALC_UTILS_ERROR(...)   printf("%s:%d:ERROR:",__FILE__,__LINE__);_ATSC3_UTILS_PRINTLN(__VA_ARGS__);
+#define _ATSC3_LLS_ALC_UTILS_WARN(...)   printf("%s:%d:ERROR:",__FILE__,__LINE__);_ATSC3_UTILS_PRINTLN(__VA_ARGS__);
+#define _ATSC3_LLS_ALC_UTILS_INFO(...)   printf("%s:%d:ERROR:",__FILE__,__LINE__);_ATSC3_UTILS_PRINTLN(__VA_ARGS__);
+#define _ATSC3_LLS_ALC_UTILS_DEBUG(...)   printf("%s:%d:ERROR:",__FILE__,__LINE__);_ATSC3_UTILS_PRINTLN(__VA_ARGS__);
+
+#define _LLS_PRINTLN(...) printf(__VA_ARGS__);printf("%s%s","\r","\n")
+#define _LLS_PRINTF(...)  printf(__VA_ARGS__);
+
+#define __LLSU_TRACE(...)
+//#define __LLSU_TRACE(...) _LLS_PRINTLN(__VA_ARGS__);
 
 
 #endif /* ATSC3_LLS_ALC_TOOLS_H_ */
