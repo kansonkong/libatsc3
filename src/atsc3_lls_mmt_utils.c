dc88a820ebffd2aa431648ee62c4565653132bea
Jason Justman <jjustman@ngbp.org>
2019-05-29 05:55:07 -0400

    fixes for ROUTE EXT_FTI (codepage=8) without alc close_object flag

--- src/atsc3_lls_mmt_utils.c
+++ src/atsc3_lls_mmt_utils.c
@@ -9,6 +9,7 @@
 
 
 int _LLSU_MMT_TRACE_ENABLED = 0;
+int _LLSU_TRACE_ENABLED = 0;
 
 lls_sls_mmt_monitor_t* lls_sls_mmt_monitor_create() {
 	lls_sls_mmt_monitor_t* lls_sls_mmt_monitor = (lls_sls_mmt_monitor_t*)calloc(1, sizeof(lls_sls_mmt_monitor_t));
