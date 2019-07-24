976a7b4e5ce4e0835657ac45b6416d37ed93499a
Jason Justman <jjustman@ngbp.org>
2019-07-23 20:50:09 -0400

    stltp reflection!

--- src/listener_tests/atsc3_stltp_listener_alp_reflector.cpp
+++ src/listener_tests/atsc3_stltp_listener_alp_reflector.cpp
@@ -1,5 +1,5 @@
 /*
- * atsc3_stltp_listener_test.c
+ * atsc3_stltp_listener_alp_reflector.c
  *
  *  Created on: Mar 16, 2019
  *      Author: jjustman
@@ -20,6 +20,7 @@
 
 #include "../atsc3_listener_udp.h"
 #include "../atsc3_stltp_parser.h"
+#include "../atsc3_alp_parser.h"
 #include "../atsc3_logging_externs.h"
 
 int PACKET_COUNTER = 0;
@@ -27,129 +28,167 @@ int PACKET_COUNTER = 0;
 uint32_t* dst_ip_addr_filter = NULL;
 uint16_t* dst_ip_port_filter = NULL;
 
+extern pcap_t* descrInject;
+
 atsc3_stltp_tunnel_packet_t* atsc3_stltp_tunnel_packet_processed = NULL;
 
 void process_packet(u_char *user, const struct pcap_pkthdr *pkthdr, const u_char *packet) {
-	udp_packet_t* udp_packet = process_packet_from_pcap(user, pkthdr, packet);
-	if(!udp_packet) {
-		return;
-	}
-
-	//dispatch for LLS extraction and dump
-	if(udp_packet->udp_flow.dst_ip_addr == *dst_ip_addr_filter && udp_packet->udp_flow.dst_port == *dst_ip_port_filter) {
-		atsc3_stltp_tunnel_packet_processed = atsc3_stltp_tunnel_packet_extract_fragment_from_udp_packet(udp_packet, atsc3_stltp_tunnel_packet_processed);
-
-		if(atsc3_stltp_tunnel_packet_processed) {
-			if(atsc3_stltp_tunnel_packet_processed->atsc3_stltp_baseband_packet && atsc3_stltp_tunnel_packet_processed->atsc3_stltp_baseband_packet->is_complete) {
-				__INFO("stltp atsc3_stltp_baseband_packet packet complete: size: %u",  atsc3_stltp_tunnel_packet_processed->atsc3_stltp_baseband_packet->payload_length);
-				atsc3_stltp_tunnel_packet_processed->atsc3_stltp_baseband_packet->
-
-			//todo - free
-			}
-			if(atsc3_stltp_tunnel_packet_processed->atsc3_stltp_preamble_packet && atsc3_stltp_tunnel_packet_processed->atsc3_stltp_preamble_packet->is_complete) {
-				__INFO("stltp atsc3_stltp_preamble_packet packet complete: size: %u",  atsc3_stltp_tunnel_packet_processed->atsc3_stltp_preamble_packet->payload_length);
-			//todo - free
-			}
-			if(atsc3_stltp_tunnel_packet_processed->atsc3_stltp_timing_management_packet && atsc3_stltp_tunnel_packet_processed->atsc3_stltp_timing_management_packet->is_complete) {
-				__INFO("stltp atsc3_stltp_timing_management_packet packet complete: size: %u",  atsc3_stltp_tunnel_packet_processed->atsc3_stltp_timing_management_packet->payload_length);
-			//todo - free
-			}
-		} else {
-            __ERROR("error processing packet: %p, size: %u",  udp_packet, udp_packet->data_length);
+    //extract our outer ip/udp/rtp packet
+    atsc3_ip_udp_rtp_packet_t* ip_udp_rtp_packet = atsc3_ip_udp_rtp_process_packet_from_pcap(user, pkthdr, packet);
+    if(!ip_udp_rtp_packet) {
+        return;
+    }
+    //TODO - add SMPTE-2022.1 FEC decoding (see fork of prompeg-decoder - https://github.com/jjustman/prompeg-decoder)
+    
+    //dispatch for LLS extraction and dump
+    if(ip_udp_rtp_packet->udp_flow.dst_ip_addr == *dst_ip_addr_filter && ip_udp_rtp_packet->udp_flow.dst_port == *dst_ip_port_filter) {
+        atsc3_stltp_tunnel_packet_processed = atsc3_stltp_raw_packet_extract_inner_from_outer_packet(ip_udp_rtp_packet, atsc3_stltp_tunnel_packet_processed);
+        
+        if(atsc3_stltp_tunnel_packet_processed) {
+            if(atsc3_stltp_tunnel_packet_processed->atsc3_stltp_baseband_packet_v.count) {
+                __INFO(">>>stltp atsc3_stltp_baseband_packet packet complete: count: %u",  atsc3_stltp_tunnel_packet_processed->atsc3_stltp_baseband_packet_v.count);
+                for(int i=0; i < atsc3_stltp_tunnel_packet_processed->atsc3_stltp_baseband_packet_v.count; i++) {
+                    atsc3_stltp_baseband_packet_t* atsc3_stltp_baseband_packet = atsc3_stltp_tunnel_packet_processed->atsc3_stltp_baseband_packet_v.data[i];
+                    __INFO("Baseband packet: src: %u.%u.%u.%u:%u, dest: %u.%u.%u.%u:%u", __toipandportnonstruct(atsc3_stltp_baseband_packet->ip_udp_rtp_packet->udp_flow.src_ip_addr, atsc3_stltp_baseband_packet->ip_udp_rtp_packet->udp_flow.src_port),
+                           __toipandportnonstruct(atsc3_stltp_baseband_packet->ip_udp_rtp_packet->udp_flow.dst_ip_addr, atsc3_stltp_baseband_packet->ip_udp_rtp_packet->udp_flow.dst_port));
+                    
+                    atsc3_alp_parse_stltp_baseband_packet(atsc3_stltp_baseband_packet);
+                    /*
+                     
+                     Injecting packets
+                     If  you have the required privileges, you can inject packets onto a network with a pcap_t for a live capture, using pcap_inject() or pcap_sendpacket().  (The
+                     two routines exist for compatibility with both OpenBSD and WinPcap; they perform the same function, but have different return values.)
+                     
+                     Routines
+                     
+                     pcap_inject(3PCAP)
+                     pcap_sendpacket(3PCAP)
+                     transmit a packet
+                     
+                     https://www.winpcap.org/docs/docs_40_2/html/group__wpcap__tut8.html
+                     **/
+                    
+                }
+            }
+            if(atsc3_stltp_tunnel_packet_processed->atsc3_stltp_preamble_packet_v.count) {
+                __INFO(">>>stltp atsc3_stltp_preamble_packet packet complete: count: %u",  atsc3_stltp_tunnel_packet_processed->atsc3_stltp_preamble_packet_v.count);
+                for(int i=0; i < atsc3_stltp_tunnel_packet_processed->atsc3_stltp_preamble_packet_v.count; i++) {
+                    atsc3_stltp_preamble_packet_t* atsc3_stltp_preamble_packet = atsc3_stltp_tunnel_packet_processed->atsc3_stltp_preamble_packet_v.data[i];
+                    //atsc3_alp_parse_stltp_preamble_packet(atsc3_stltp_preamble_packet);
+                }
+            }
+            if(atsc3_stltp_tunnel_packet_processed->atsc3_stltp_timing_management_packet_v.count) {
+                __INFO(">>>stltp atsc3_stltp_timing_management_packet packet complete: count: %u",  atsc3_stltp_tunnel_packet_processed->atsc3_stltp_timing_management_packet_v.count);
+                for(int i=0; i < atsc3_stltp_tunnel_packet_processed->atsc3_stltp_timing_management_packet_v.count; i++) {
+                    atsc3_stltp_timing_management_packet_t* atsc3_stltp_timing_management_packet = atsc3_stltp_tunnel_packet_processed->atsc3_stltp_timing_management_packet_v.data[i];
+                    //atsc3_alp_parse_stltp_baseband_packet(atsc3_stltp_baseband_packet);
+                }
+            }
             
-		}
-
-
-	}
-
-	if(udp_packet->data) {
-		free(udp_packet->data);
-		udp_packet->data = NULL;
-	}
-
-	if(udp_packet) {
-		free(udp_packet);
-		udp_packet = NULL;
-	}
+            atsc3_stltp_tunnel_packet_clear_completed_inner_packets(atsc3_stltp_tunnel_packet_processed);
+            
+        } else {
+            __ERROR("error processing packet: %p, size: %u",  ip_udp_rtp_packet, ip_udp_rtp_packet->data->p_size);
+        }
+    }
+    
+    
+    atsc3_ip_udp_rtp_packet_and_data_free(&ip_udp_rtp_packet);
 }
 
 int main(int argc,char **argv) {
-
+    
+    
+    _IP_UDP_RTP_PARSER_DEBUG_ENABLED = 1;
     char *dev;
+    char *devInject;
     char *filter_dst_ip = NULL;
     char *filter_dst_port = NULL;
     char *filter_packet_id = NULL;
-
+    
     int dst_port_filter_int;
     int dst_ip_port_filter_int;
     int dst_packet_id_filter_int;
-
+    
     char errbuf[PCAP_ERRBUF_SIZE];
+    char errbufInject[PCAP_ERRBUF_SIZE];
 
     pcap_t* descr;
     struct bpf_program fp;
     bpf_u_int32 maskp;
     bpf_u_int32 netp;
+    
+    struct bpf_program fpInject;
+    bpf_u_int32 maskpInject;
+    bpf_u_int32 netpInject;
 
-    if(argc != 4) {
-    	println("%s - a udp mulitcast listener test harness for atsc3 stltp payloads", argv[0]);
-    	println("---");
-    	println("args: dev ip port");
-    	println(" dev : device to listen for udp multicast");
-    	println(" ip  : ip address for stltp");
-    	println(" port: port for single stltp");
-
-		exit(1);
+    
+    if(argc != 5) {
+        println("%s - an atsc3 stltp udp mulitcast reflector ", argv[0]);
+        println("---");
+        println("args: devListen ip port devInject");
+        println(" devListen : device to listen for stltp udp multicast");
+        println(" ip        : ip address for stltp");
+        println(" port      : port for single stltp");
+        println(" devInject : device to inject for ALP IP reflection");
+        
+        
+        exit(1);
     } else {
-    	dev = argv[1];
-		filter_dst_ip = argv[2];
-		filter_dst_port = argv[3];
-
-
-    	//parse ip
-    	dst_ip_addr_filter = (uint32_t*)calloc(1, sizeof(uint32_t));
-		char* pch = strtok (filter_dst_ip,".");
-		int offset = 24;
-		while (pch != NULL && offset>=0) {
-			uint8_t octet = atoi(pch);
-			*dst_ip_addr_filter |= octet << offset;
-			offset-=8;
-			pch = strtok (NULL, ".");
-		}
-
-		//parse port
-
-		dst_port_filter_int = atoi(filter_dst_port);
-		dst_ip_port_filter = (uint16_t*)calloc(1, sizeof(uint16_t));
-		*dst_ip_port_filter |= dst_port_filter_int & 0xFFFF;
-
+        dev = argv[1];
+        filter_dst_ip = argv[2];
+        filter_dst_port = argv[3];
+        devInject = argv[4];
+        
+        //parse ip
+        dst_ip_addr_filter = (uint32_t*)calloc(1, sizeof(uint32_t));
+        char* pch = strtok (filter_dst_ip,".");
+        int offset = 24;
+        while (pch != NULL && offset>=0) {
+            uint8_t octet = atoi(pch);
+            *dst_ip_addr_filter |= octet << offset;
+            offset-=8;
+            pch = strtok (NULL, ".");
+        }
+        
+        //parse port
+        
+        dst_port_filter_int = atoi(filter_dst_port);
+        dst_ip_port_filter = (uint16_t*)calloc(1, sizeof(uint16_t));
+        *dst_ip_port_filter |= dst_port_filter_int & 0xFFFF;
+        
     }
-
-
-	println("%s - a udp mulitcast listener test harness for atsc3 stltp payloads, listening on dev: %s", argv[0], argv[1]);
-
+    
+    
+    println("%s -an atsc3 stltp udp mulitcast reflector , listening on dev: %s, refleting: %s", argv[0], dev, devInject);
+    
     pcap_lookupnet(dev, &netp, &maskp, errbuf);
     descr = pcap_open_live(dev, MAX_PCAP_LEN, 1, 0, errbuf);
-
+    
     if(descr == NULL) {
         printf("pcap_open_live(): %s",errbuf);
         exit(1);
     }
-
+    
     char filter[] = "udp";
     if(pcap_compile(descr,&fp, filter,0,netp) == -1) {
         fprintf(stderr,"Error calling pcap_compile");
         exit(1);
     }
-
+    
     if(pcap_setfilter(descr,&fp) == -1) {
         fprintf(stderr,"Error setting filter");
         exit(1);
-
+        
     }
+   
+    //inject
+    pcap_lookupnet(devInject, &netpInject, &maskpInject, errbufInject);
+    descrInject = pcap_open_live(devInject, MAX_PCAP_LEN, 1, 0, errbufInject);
 
-    pcap_loop(descr,-1,process_packet,NULL);
-
+    
+    pcap_loop(descr, -1, process_packet, NULL);
+    
     return 0;
 }
 
