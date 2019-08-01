82c08bb5548191d75f381fc9ea90450d7b477577
Jason Justman <jjustman@ngbp.org>
2019-08-01 05:23:27 -0400

    closer for full reflection of ALP with refactoring

--- src/atsc3_stltp_types.c
+++ src/atsc3_stltp_types.c
@@ -49,14 +49,12 @@ atsc3_stltp_baseband_packet_t* atsc3_stltp_baseband_packet_new_and_init(atsc3_st
     
     //ref ip_udp_rtp_packet_outer w/ rtp_header_outer
     atsc3_stltp_baseband_packet->ip_udp_rtp_packet_outer = atsc3_ip_udp_rtp_packet_duplicate(atsc3_stltp_tunnel_packet->ip_udp_rtp_packet_inner);
-    atsc3_stltp_baseband_packet->rtp_header_outer = atsc3_stltp_baseband_packet->ip_udp_rtp_packet_outer->rtp_header;
 
     //ref ip_udp_rtp_packet_inner w/ rtp_header_inner
     atsc3_stltp_baseband_packet->ip_udp_rtp_packet_inner = atsc3_ip_udp_rtp_packet_duplicate(atsc3_stltp_tunnel_packet->ip_udp_rtp_packet_inner);
-    atsc3_stltp_baseband_packet->rtp_header_inner = atsc3_stltp_baseband_packet->ip_udp_rtp_packet_inner->rtp_header;
     
     //TODO: inner packet size, only if we are from a marker
-    atsc3_stltp_baseband_packet->payload_length = atsc3_stltp_baseband_packet->rtp_header_inner->packet_offset;
+    atsc3_stltp_baseband_packet->payload_length = atsc3_stltp_baseband_packet->ip_udp_rtp_packet_inner->rtp_header->packet_offset;
     
     return atsc3_stltp_baseband_packet;
 }
@@ -209,9 +207,6 @@ void atsc3_stltp_baseband_packet_free_v(atsc3_stltp_baseband_packet_t* atsc3_stl
 
         atsc3_ip_udp_rtp_packet_destroy_outer_inner(&atsc3_stltp_baseband_packet->ip_udp_rtp_packet_outer, &atsc3_stltp_baseband_packet->ip_udp_rtp_packet_inner);
         
-        atsc3_stltp_baseband_packet->rtp_header_outer = NULL; //atsc3_ip_udp_rtp_packet_free will free our shared pointer
-        atsc3_stltp_baseband_packet->rtp_header_inner = NULL; //atsc3_ip_udp_rtp_packet_free will free our shared pointer
-        
         //TODO: move this over to block_t
         if(atsc3_stltp_baseband_packet->payload) {
             free(atsc3_stltp_baseband_packet->payload);
@@ -225,7 +220,7 @@ void atsc3_stltp_baseband_packet_free_v(atsc3_stltp_baseband_packet_t* atsc3_stl
  free only the ip_udp_rtp_packet outer/inner data payloads, as we will have rebuilt what the baseband packet needs in its ->payload
  do not remove the rtp_header outer and inner, as this data may be needed for coorelation of the timestamp field(s)
  */
-void atsc3_stltp_baseband_packet_free_inner_outer_data(atsc3_stltp_baseband_packet_t* atsc3_stltp_baseband_packet) {
+void atsc3_stltp_baseband_packet_free_outer_inner_data(atsc3_stltp_baseband_packet_t* atsc3_stltp_baseband_packet) {
     
     if(atsc3_stltp_baseband_packet->ip_udp_rtp_packet_outer && atsc3_stltp_baseband_packet->ip_udp_rtp_packet_outer->data) {
         block_Release(&atsc3_stltp_baseband_packet->ip_udp_rtp_packet_outer->data);
@@ -236,18 +231,43 @@ void atsc3_stltp_baseband_packet_free_inner_outer_data(atsc3_stltp_baseband_pack
     }
 }
 
+//copy/paste warning - atsc3_stltp_preamble_packet
+void atsc3_stltp_preamble_packet_free_outer_inner_data(atsc3_stltp_preamble_packet_t* atsc3_stltp_preamble_packet) {
+    
+    if(atsc3_stltp_preamble_packet->ip_udp_rtp_packet_outer && atsc3_stltp_preamble_packet->ip_udp_rtp_packet_outer->data) {
+        block_Release(&atsc3_stltp_preamble_packet->ip_udp_rtp_packet_outer->data);
+    }
+    
+    if(atsc3_stltp_preamble_packet->ip_udp_rtp_packet_inner && atsc3_stltp_preamble_packet->ip_udp_rtp_packet_inner->data) {
+        block_Release(&atsc3_stltp_preamble_packet->ip_udp_rtp_packet_inner->data);
+    }
+}
+
+//copy/paste warning - atsc3_stltp_timing_management_packet
+void atsc3_stltp_timing_management_packet_free_outer_inner_data(atsc3_stltp_timing_management_packet_t* atsc3_stltp_timing_management_packet) {
+    
+    if(atsc3_stltp_timing_management_packet->ip_udp_rtp_packet_outer && atsc3_stltp_timing_management_packet->ip_udp_rtp_packet_outer->data) {
+        block_Release(&atsc3_stltp_timing_management_packet->ip_udp_rtp_packet_outer->data);
+    }
+    
+    if(atsc3_stltp_timing_management_packet->ip_udp_rtp_packet_inner && atsc3_stltp_timing_management_packet->ip_udp_rtp_packet_inner->data) {
+        block_Release(&atsc3_stltp_timing_management_packet->ip_udp_rtp_packet_inner->data);
+    }
+}
+
+
+
 void atsc3_stltp_preamble_packet_free_v(atsc3_stltp_preamble_packet_t* atsc3_stltp_preamble_packet) {
     if(atsc3_stltp_preamble_packet) {
-        if(atsc3_stltp_preamble_packet->rtp_header) {
-            __STLTP_TYPES_TRACE("atsc3_stltp_baseband_packet_free: freeing atsc3_stltp_preamble_packet->rtp_header: %p", atsc3_stltp_preamble_packet->rtp_header);
-            atsc3_rtp_header_free(&atsc3_stltp_preamble_packet->rtp_header);
-        }
+        
+        //this shoudl be all boilerplate
+        atsc3_ip_udp_rtp_packet_free(&atsc3_stltp_preamble_packet->ip_udp_rtp_packet_outer);
+        atsc3_ip_udp_rtp_packet_free(&atsc3_stltp_preamble_packet->ip_udp_rtp_packet_inner);
+        //this shoudl be all boilerplate
+    
         if(atsc3_stltp_preamble_packet->payload) {
             free(atsc3_stltp_preamble_packet->payload);
-            atsc3_stltp_preamble_packet->payload = NULL;
-        }
-        if(atsc3_stltp_preamble_packet->ip_udp_rtp_packet) {
-            atsc3_ip_udp_rtp_packet_free(&atsc3_stltp_preamble_packet->ip_udp_rtp_packet);
+            atsc3_stltp_preamble_packet->payload = NULL;        
         }
         //let vector_v initiate the pointer free
         //free(atsc3_stltp_preamble_packet);
@@ -264,13 +284,14 @@ void atsc3_stltp_preamble_packet_free_v(atsc3_stltp_preamble_packet_t* atsc3_stl
 
  **/
 void atsc3_stltp_timing_management_packet_free_v(atsc3_stltp_timing_management_packet_t* atsc3_stltp_timing_management_packet) {
-    if(atsc3_stltp_timing_management_packet) {    
+    if(atsc3_stltp_timing_management_packet) {
+        
         //this shoudl be all boilerplate
-        atsc3_rtp_header_free(&atsc3_stltp_timing_management_packet->rtp_header_outer);
         atsc3_ip_udp_rtp_packet_free(&atsc3_stltp_timing_management_packet->ip_udp_rtp_packet_outer);
-        atsc3_rtp_header_free(&atsc3_stltp_timing_management_packet->rtp_header_inner);
         atsc3_ip_udp_rtp_packet_free(&atsc3_stltp_timing_management_packet->ip_udp_rtp_packet_inner);
+        //this shoudl be all boilerplate
 
+        //stltp CTP specific attibutes here
         if(atsc3_stltp_timing_management_packet->payload) {
             free(atsc3_stltp_timing_management_packet->payload);
             atsc3_stltp_timing_management_packet->payload = NULL;
