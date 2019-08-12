a5599e0d9ea9a0891992abc3dd366970a0b01589
Jason Justman <jjustman@ngbp.org>
2019-08-12 08:11:15 -0700

    feeing mpu_timestamp_descriptor

--- src/test/atsc3_mmt_signalling_packet_test.c
+++ src/test/atsc3_mmt_signalling_packet_test.c
@@ -45,23 +45,16 @@ block_t* get_ip_frame_payload_from_raw_ether_filename(const char* file_name) {
 }
 
 
-
-int test_parse_mp_item() {
-
-	return 0;
-}
-
 /*
- * 239.255.1.1.49152.6426.14453.mpt.0.bin
- * 210 bytes
+ * 239.255.1.1.49152.6426.14247.mpt.3.bin
+ * 116 bytes
  *
+ * MPT - single entry
  */
 
+int test_parse_mp_table_single_item_no_factory() {
 
-int test_parse_mp_table_no_factory() {
-
-
-	const char* TEST_ATSC3_MMT_MESSAGE_FILENAME = "testdata/mmt/signalling_info/239.255.1.1.49152.6426.14453.mpt.0.bin";
+	const char* TEST_ATSC3_MMT_MESSAGE_FILENAME = "testdata/mmt/signalling_info/239.255.1.1.49152.6426.14247.mpt.3.bin";
 	__MMSM_INFO("test_parse_atsc3_mmt_message_no_factoy: opening file: %s", TEST_ATSC3_MMT_MESSAGE_FILENAME);
 
 	block_t* test_atsc3_mmt_message_payload = get_ip_frame_payload_from_raw_ether_filename(TEST_ATSC3_MMT_MESSAGE_FILENAME);
@@ -91,6 +84,55 @@ int test_parse_mp_table_no_factory() {
 	block_Release(&test_atsc3_mmt_message_payload);
 
 	return 0;
+}
+
+/*
+ * 239.255.1.1.49152.6426.14453.mpt.0.bin
+ * 210 bytes
+ *
+==34821== LEAK SUMMARY:
+==34821==    definitely lost: 5,276 bytes in 11 blocks
+==34821==    indirectly lost: 6,972 bytes in 11 blocks
+==34821==      possibly lost: 48 bytes in 2 blocks
+==34821==    still reachable: 384 bytes in 9 blocks
+==34821==         suppressed: 11,033 bytes in 143 blocks
+==34821== Reachable blocks (those to which a pointer was found) are not shown.
+==34821== To see them, rerun with: --leak-check=full --show-leak-kinds=all
+ *
+ *
+ */
+
+
+int test_parse_mp_table_no_factory() {
+
+	const char* TEST_ATSC3_MMT_MESSAGE_FILENAME = "testdata/mmt/signalling_info/239.255.1.1.49152.6426.14453.mpt.0.bin";
+	__MMSM_INFO("test_parse_atsc3_mmt_message_no_factoy: opening file: %s", TEST_ATSC3_MMT_MESSAGE_FILENAME);
+
+	block_t* test_atsc3_mmt_message_payload = get_ip_frame_payload_from_raw_ether_filename(TEST_ATSC3_MMT_MESSAGE_FILENAME);
+	assert(test_atsc3_mmt_message_payload);
+	__MMSM_INFO("test_parse_atsc3_mmt_message_no_factoy: i_pos: %d, p_size: %d", test_atsc3_mmt_message_payload->i_pos, test_atsc3_mmt_message_payload->p_size);
+
+	//START: method under test
+
+	for(int i=0; i < LEAK_CHECK_RUN_COUNT; i++) {
+		block_Rewind(test_atsc3_mmt_message_payload);
+		mmtp_packet_header_t* mmtp_packet_header = mmtp_packet_header_parse_from_block_t(test_atsc3_mmt_message_payload);
+
+		mmtp_packet_header_dump(mmtp_packet_header);
+
+		mmtp_signalling_packet_t* mmtp_signalling_packet = mmt_signalling_message_parse_packet_header(mmtp_packet_header, test_atsc3_mmt_message_payload);
+		uint8_t mmt_SI_messages_processed = mmt_signalling_message_parse_packet(mmtp_signalling_packet, test_atsc3_mmt_message_payload);
+
+		mmt_signalling_message_dump(mmtp_signalling_packet);
+
+		//free our alloc(s)
+		mmt_signalling_message_free(&mmtp_signalling_packet);
+		mmtp_packet_header_free(&mmtp_packet_header);
+	}
+
+
+	//END: method under test
+	block_Release(&test_atsc3_mmt_message_payload);
 
 	return 0;
 }
@@ -171,8 +213,13 @@ int main(int argc, char* argv[] ) {
 		test_parse_atsc3_mmt_message_no_factoy();
 	}
 
-	if(true) {
+	if(false) {
 		__MMSM_INFO("test_parse_mp_table_no_factory");
 		test_parse_mp_table_no_factory();
+	}
+
+	if(true) {
+		__MMSM_INFO("test_parse_mp_table_single_item_no_factory");
+		test_parse_mp_table_single_item_no_factory();
 	}
 }
