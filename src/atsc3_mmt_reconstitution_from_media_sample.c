6085629c4f91f909bee7bcbc29bf9138670774a8
Jason Justman <jjustman@ngbp.org>
2019-08-31 10:52:38 -0700

    isobmff re-assembly now working agian

--- src/atsc3_mmt_reconstitution_from_media_sample.c
+++ src/atsc3_mmt_reconstitution_from_media_sample.c
@@ -109,16 +109,15 @@ void mmtp_process_from_payload(mmtp_mpu_packet_t* mmtp_mpu_packet,
                                                            mmtp_packet_id_packets_container);
 
 						lls_sls_monitor_buffer_isobmff_intermediate_mmt_file_dump(lls_sls_monitor_buffer_isobmff_pending_mux, "mpu/",
-								matching_lls_slt_mmt_session->last_udp_flow_packet_id_mpu_sequence_tuple_audio->mpu_sequence_number,
-								"a.orig");
+                                                                                 mmtp_mpu_packet->mmtp_packet_id,
+                                                                                 matching_lls_slt_mmt_session->last_udp_flow_packet_id_mpu_sequence_tuple_audio->mpu_sequence_number,
+                                                                                 "a.orig");
 
 						atsc3_isobmff_rebuild_track_mpu_from_sample_data(lls_sls_monitor_buffer_isobmff_pending_mux);
 						ls_sls_monitor_buffer_isobmff_mmt_mpu_rebuilt_file_dump(lls_sls_monitor_buffer_isobmff_pending_mux, "mpu/",
 														matching_lls_slt_mmt_session->last_udp_flow_packet_id_mpu_sequence_tuple_audio->mpu_sequence_number,
 														"a.rebuilt");
                         
-//                        mpu_sequence_number_mmtp_mpu_packet_collection_t* mpu_sequence_number_mmtp_mpu_packet_collection = mmtp_packet_id_packets_container_find_mpu_sequence_number_mmtp_mpu_packet_collection_from_mpu_sequence_number(mmtp_packet_id_packets_container, matching_lls_slt_mmt_session->last_udp_flow_packet_id_mpu_sequence_tuple_audio->mpu_sequence_number);
-//                        mpu_sequence_number_mmtp_mpu_packet_collection_free(&mpu_sequence_number_mmtp_mpu_packet_collection);
                         mpu_sequence_number_mmtp_mpu_packet_collection_t* mpu_sequence_number_mmtp_mpu_packet_collection = mmtp_packet_id_packets_container_find_mpu_sequence_number_mmtp_mpu_packet_collection_from_mpu_sequence_number(mmtp_packet_id_packets_container, matching_lls_slt_mmt_session->last_udp_flow_packet_id_mpu_sequence_tuple_audio->mpu_sequence_number);
                         mmtp_packet_id_packets_container_remove_mpu_sequence_number_mmtp_mpu_packet_collection(mmtp_packet_id_packets_container, mpu_sequence_number_mmtp_mpu_packet_collection);
 					}
@@ -137,6 +136,7 @@ void mmtp_process_from_payload(mmtp_mpu_packet_t* mmtp_mpu_packet,
                                                            mmtp_packet_id_packets_container);
 
 						lls_sls_monitor_buffer_isobmff_intermediate_mmt_file_dump(lls_sls_monitor_buffer_isobmff_pending_mux, "mpu/",
+                                                        mmtp_mpu_packet->mmtp_packet_id,
 														matching_lls_slt_mmt_session->last_udp_flow_packet_id_mpu_sequence_tuple_video->mpu_sequence_number,
 														"v.orig");
 						atsc3_isobmff_rebuild_track_mpu_from_sample_data(lls_sls_monitor_buffer_isobmff_pending_mux);
