294135ff4c6f5df539c99d978573812e8361efa4
Jason Justman <jjustman@ngbp.org>
2019-09-09 19:26:25 -0400

    updating linux build

--- src/endianess.c
+++ src/endianess.c
@@ -1,20 +1,27 @@
 /*
  * endianess.h
  */
+#include <arpa/inet.h>
 
 #ifndef htonll
 
-unsigned long long htonll(unsigned long long v) {
-    union { unsigned long lv[2]; unsigned long long llv; } u;
-    u.lv[0] = htonl(v >> 32);
-    u.lv[1] = htonl(v & 0xFFFFFFFFULL);
-    return u.llv;
-}
+#define htonll(x) ((((uint64_t)htonl(x)) << 32) + htonl((x) >> 32))
 
-unsigned long long ntohll(unsigned long long v) {
-    union { unsigned long lv[2]; unsigned long long llv; } u;
-    u.llv = v;
-    return ((unsigned long long)ntohl(u.lv[0]) << 32) | (unsigned long long)ntohl(u.lv[1]);
-}
+//unsigned long long htonll(unsigned long long v) {
+//#define htonll(v)						\
+//  (union { unsigned long lv[2]; unsigned long long llv; } u;	\
+//    u.lv[0] = htonl(v >> 32); \
+//    u.lv[1] = htonl(v & 0xFFFFFFFFULL); \
+//    return u.llv; \
+//)
+
+#define ntohll(x) ((((uint64_t)ntohl(x)) << 32) + ntohl((x) >> 32))
+
+//unsigned long long ntohll(unsigned long long v) {
+//#define ntohll(v)					     \
+//  (union { unsigned long lv[2]; unsigned long long llv; } u; \
+//    u.llv = v; \
+//   return ((unsigned long long)ntohl(u.lv[0]) << 32) | (unsigned long long)ntohl(u.lv[1]); \
+//)
 
 #endif
