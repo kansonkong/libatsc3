bdcfdcbfbaf52b881d98436a4b0ca2854cbcda1d
Jason Justman <jjustman@ngbp.org>
2019-10-03 05:19:24 -0700

    success for first callback flow!
    atsc3_mmt_context_mfu_de:
    49:DEBUG:1570105148.5749:atsc3_mmt_mpu_on_sequence_number_change:
    packet_id: 10, from 7622, to 7623
    atsc3_mmt_context_mfu_de:
    49:DEBUG:1570105149.0035:atsc3_mmt_mpu_on_sequence_number_change:
    packet_id: 11, from 7622, to 7623
    atsc3_mmt_context_mfu_de:
    49:DEBUG:1570105149.5815:atsc3_mmt_mpu_on_sequence_number_change:
    packet_id: 10, from 7623, to 7624
    atsc3_mmt_context_mfu_de:
    49:DEBUG:1570105149.9871:atsc3_mmt_mpu_on_sequence_number_change:
    packet_id: 11, from 7623, to 7624
    atsc3_mmt_context_mfu_de:
    49:DEBUG:1570105152.3867:atsc3_mmt_mpu_on_sequence_number_change:
    packet_id: 10, from 7624, to 7626
    atsc3_mmt_context_mfu_de:
    49:DEBUG:1570105152.3869:atsc3_mmt_mpu_on_sequence_number_change:
    packet_id: 10, from 7626, to 7627
    atsc3_mmt_context_mfu_de:
    49:DEBUG:1570105152.4076:atsc3_mmt_mpu_on_sequence_number_change:
    packet_id: 11, from 7624, to 7626
    atsc3_mmt_context_mfu_de:
    49:DEBUG:1570105152.7585:atsc3_mmt_mpu_on_sequence_number_change:
    packet_id: 11, from 7626, to 7627
    atsc3_mmt_context_mfu_de:
    49:DEBUG:1570105154.3000:atsc3_mmt_mpu_on_sequence_number_change:
    packet_id: 10, from 7627, to 7629
    atsc3_mmt_context_mfu_de:
    49:DEBUG:1570105154.3003:atsc3_mmt_mpu_on_sequence_number_change:
    packet_id: 11, from 7627, to 7628

--- src/tools/atsc3_mmt_listener_to_http_hls_fmp4.cpp
+++ src/tools/atsc3_mmt_listener_to_http_hls_fmp4.cpp
@@ -685,6 +685,7 @@ void process_packet(u_char *user, const struct pcap_pkthdr *pkthdr, const u_char
 		return udp_packet_free(&udp_packet);
 	}
 
+	//auto-monitor code here for MMT
 	if(udp_packet->udp_flow.dst_ip_addr == LLS_DST_ADDR && udp_packet->udp_flow.dst_port == LLS_DST_PORT) {
 		//process as lls.sst, dont free as we keep track of our object in the lls_slt_monitor
         lls_table_t* lls_table = lls_table_create_or_update_from_lls_slt_monitor(lls_slt_monitor, udp_packet->data);
