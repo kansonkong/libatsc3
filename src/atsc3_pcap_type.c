1fe2ef9222a7858aa7fdf7122b49111b595a12a5
Jason Justman <jjustman@ngbp.org>
2019-10-10 14:50:53 -0500

    adding in pcap replay test harness

--- src/atsc3_pcap_type.c
+++ src/atsc3_pcap_type.c
@@ -37,15 +37,49 @@ atsc3_pcap_replay_context_t* atsc3_pcap_replay_open_filename(const char* pcap_fi
 		return NULL;
 	}
 
-	atsc3_pcap_replay_context->len = st.st_size;
+	atsc3_pcap_replay_context->pcap_file_len = st.st_size;
+	atsc3_pcap_replay_context->pcap_file_pos = 0;
+
 	atsc3_pcap_replay_context->pcap_fp = fopen(atsc3_pcap_replay_context->pcap_file_name, "r");
 	if(!atsc3_pcap_replay_context->pcap_fp) {
 		return NULL;
+	}
 
 	return atsc3_pcap_replay_context;
 }
 
-atsc3_pcap_replay_context_t* atsc3_pcap_replay_open_filename(atsc3_pcap_replay_context_t* atsc3_pcap_replay_context_to_iterate) {
-	if(atsc3_pcap_replay_context_to_iterate->pos + ATSC3_PCAP_MIN_GLOBAL_AND_PACKET_AND_ETH_HEADER_LENGTH > atsc3_pcap_replay_context_to_iterate->len)
+atsc3_pcap_replay_context_t* atsc3_pcap_replay_iterate_packet(atsc3_pcap_replay_context_t* atsc3_pcap_replay_context_to_iterate) {
+	if(atsc3_pcap_replay_context_to_iterate->pcap_file_pos + ATSC3_PCAP_MIN_GLOBAL_AND_PACKET_AND_ETH_HEADER_LENGTH > atsc3_pcap_replay_context_to_iterate->pcap_file_len) {
+		return NULL;
+	}
 
+	if(!atsc3_pcap_replay_context_to_iterate->pcap_fp) {
+		return NULL;
+	}
+
+	if(atsc3_pcap_replay_context_to_iterate->atsc3_pcap_packet_instance.current_pcap_packet) {
+		block_Destroy(&atsc3_pcap_replay_context_to_iterate->atsc3_pcap_packet_instance.current_pcap_packet);
+	}
+
+	memset(&atsc3_pcap_replay_context_to_iterate->atsc3_pcap_packet_instance, 0, sizeof(atsc3_pcap_packet_instance_t));
+
+	//read our global header first
+	fread((void*)&atsc3_pcap_replay_context_to_iterate->atsc3_pcap_packet_instance.atsc3_pcap_global_header, sizeof(atsc3_pcap_global_header_t), 1, atsc3_pcap_replay_context_to_iterate->pcap_fp);
+	fread((void*)&atsc3_pcap_replay_context_to_iterate->atsc3_pcap_packet_instance.atsc3_pcap_packet_header, sizeof(atsc3_pcap_packet_header_t), 1, atsc3_pcap_replay_context_to_iterate->pcap_fp);
+
+	atsc3_pcap_replay_context_to_iterate->pcap_file_pos += sizeof(atsc3_pcap_global_header_t) + sizeof(atsc3_pcap_packet_header_t);
+
+
+	atsc3_pcap_replay_context_to_iterate->atsc3_pcap_packet_instance.current_pcap_packet = block_Alloc(atsc3_pcap_replay_context_to_iterate->atsc3_pcap_packet_instance.atsc3_pcap_packet_header.incl_len);
+	fread((void*)block_Get(atsc3_pcap_replay_context_to_iterate->atsc3_pcap_packet_instance.current_pcap_packet->p_buffer), atsc3_pcap_replay_context_to_iterate->atsc3_pcap_packet_instance.atsc3_pcap_packet_header.incl_len, 1, atsc3_pcap_replay_context_to_iterate->pcap_fp);
+
+	atsc3_pcap_replay_context_to_iterate->pcap_read_packet_count++;
+
+	return atsc3_pcap_replay_context_to_iterate;
+}
+
+
+atsc3_pcap_replay_context_t* atsc3_pcap_replay_usleep_packet(atsc3_pcap_replay_context_t* atsc3_pcap_replay_context_to_iterate) {
+
+	return atsc3_pcap_replay_context_to_iterate;
 }
