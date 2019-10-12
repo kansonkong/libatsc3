86843f2730d7c1e4e97a8479322f47211cd54f76
Jason Justman <jjustman@ngbp.org>
2019-10-12 04:06:27 -0700

    adding mfu sample data

--- src/atsc3_hevc_nal_extractor.h
+++ src/atsc3_hevc_nal_extractor.h
@@ -256,6 +256,8 @@ video_decoder_configuration_record_t* atsc3_avc1_hevc_nal_extractor_parse_from_m
 
 video_decoder_configuration_record_t* video_decoder_configuration_record_new();
 block_t* atsc3_hevc_extract_extradata_nals_combined_ffmpegImpl(block_t* hvcc_box);
+block_t* atsc3_hevc_extract_mp4toannexb_filter_ffmpegImpl(block_t* sample, block_t* last_extradata_NAL_parsed);
+
 
 void atsc3_hevc_decoder_configuration_record_dump(hevc_decoder_configuration_record_t* hevc_decoder_configuration_record);
 void atsc3_hevc_nals_record_dump(const char* label, block_t* block);
