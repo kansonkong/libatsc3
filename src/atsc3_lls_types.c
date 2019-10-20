735fd58902fff4ae81b36264208c6f72da0980f4
Jason Justman <jjustman@ngbp.org>
2019-10-19 20:40:19 -0700

    JNI for MMT SLS monitoring service id change

--- src/atsc3_lls_types.c
+++ src/atsc3_lls_types.c
@@ -7,6 +7,8 @@
 
 #include "atsc3_lls_types.h"
 
+//vector impl's for lls slt management
+
 //atsc3_lls_slt_service
 ATSC3_VECTOR_BUILDER_METHODS_IMPLEMENTATION(atsc3_lls_slt_service, atsc3_slt_simulcast_tsid);
 ATSC3_VECTOR_BUILDER_METHODS_IMPLEMENTATION(atsc3_lls_slt_service, atsc3_slt_svc_capabilities);
@@ -28,6 +30,10 @@ ATSC3_VECTOR_BUILDER_METHODS_IMPLEMENTATION(lls_sls_alc_session_flows, lls_sls_a
 
 ATSC3_VECTOR_BUILDER_METHODS_IMPLEMENTATION(lls_sls_mmt_session_flows, lls_sls_mmt_session);
 
+
+//lls_slt_service_id_group_id_cache
+ATSC3_VECTOR_BUILDER_METHODS_IMPLEMENTATION(lls_slt_service_id_group_id_cache, atsc3_lls_slt_service_cache);
+
 //lls_slt_monitor
 ATSC3_VECTOR_BUILDER_METHODS_IMPLEMENTATION(lls_slt_monitor, lls_slt_service_id);
 
@@ -37,16 +43,7 @@ ATSC3_VECTOR_BUILDER_METHODS_IMPLEMENTATION(lls_slt_monitor, lls_sls_mmt_session
 ATSC3_VECTOR_BUILDER_METHODS_PARENT_IMPLEMENTATION(lls_slt_monitor);
 ATSC3_VECTOR_BUILDER_METHODS_IMPLEMENTATION(lls_slt_monitor, lls_sls_alc_monitor);
 ATSC3_VECTOR_BUILDER_METHODS_IMPLEMENTATION(lls_slt_monitor, lls_sls_alc_session_flows);
-
-lls_slt_service_id_t* lls_slt_service_id_new_from_atsc3_lls_slt_service(atsc3_lls_slt_service_t* atsc3_lls_slt_service) {
-	lls_slt_service_id_t* lls_slt_service_id = lls_slt_service_id_new();
-	lls_slt_service_id->service_id = atsc3_lls_slt_service->service_id;
-	gettimeofday(&lls_slt_service_id->time_added, NULL);
-	gettimeofday(&lls_slt_service_id->time_last_slt_update, NULL);
-
-	return lls_slt_service_id;
-}
-
+ATSC3_VECTOR_BUILDER_METHODS_IMPLEMENTATION(lls_slt_monitor, lls_slt_service_id_group_id_cache);
 
 /**
  *
@@ -87,7 +84,75 @@ ATSC3_VECTOR_BUILDER_METHODS_ITEM_FREE(lls_sls_alc_monitor);
 ATSC3_VECTOR_BUILDER_METHODS_ITEM_FREE(lls_sls_alc_session_flows);
 
 
+/*
+ *
+ * accessors for lls_slt_monitor and group_id/slt_id caching
+ */
 
+lls_slt_service_id_t* lls_slt_service_id_new_from_atsc3_lls_slt_service(atsc3_lls_slt_service_t* atsc3_lls_slt_service) {
+	lls_slt_service_id_t* lls_slt_service_id = lls_slt_service_id_new();
+	lls_slt_service_id->service_id = atsc3_lls_slt_service->service_id;
+	gettimeofday(&lls_slt_service_id->time_added, NULL);
+	gettimeofday(&lls_slt_service_id->time_last_slt_update, NULL);
+
+	return lls_slt_service_id;
+}
+
+lls_slt_service_id_group_id_cache_t* lls_slt_monitor_find_lls_slt_service_id_group_id_cache_from_lls_group_id(lls_slt_monitor_t* lls_slt_monitor, uint8_t lls_group_id) {
+    lls_slt_service_id_group_id_cache_t* lls_slt_service_id_group_id_cache = NULL;
+    for(int i=0; i < lls_slt_monitor->lls_slt_service_id_group_id_cache_v.count; i++) {
+        lls_slt_service_id_group_id_cache = lls_slt_monitor->lls_slt_service_id_group_id_cache_v.data[i];
+        if(lls_slt_service_id_group_id_cache->lls_group_id == lls_group_id) {
+            return lls_slt_service_id_group_id_cache;
+        }
+    }
+    return NULL;
+}
+
+lls_slt_service_id_group_id_cache_t* lls_slt_monitor_find_or_create_lls_slt_service_id_group_id_cache_from_lls_group_id(lls_slt_monitor_t* lls_slt_monitor, uint8_t lls_group_id) {
+    lls_slt_service_id_group_id_cache_t* lls_slt_service_id_group_id_cache = lls_slt_monitor_find_lls_slt_service_id_group_id_cache_from_lls_group_id(lls_slt_monitor, lls_group_id);
+    if(!lls_slt_service_id_group_id_cache) {
+        lls_slt_service_id_group_id_cache = lls_slt_service_id_group_id_cache_new();
+        lls_slt_service_id_group_id_cache->lls_group_id = lls_group_id;
+        lls_slt_monitor_add_lls_slt_service_id_group_id_cache(lls_slt_monitor, lls_slt_service_id_group_id_cache);
+    }
+
+    return lls_slt_service_id_group_id_cache;
+}
+
+
+atsc3_lls_slt_service_t* lls_slt_monitor_add_lls_slt_service_id_group_id_cache_entry(lls_slt_monitor_t* lls_slt_monitor, uint16_t lls_group_id, atsc3_lls_slt_service_t* atsc3_lls_slt_service) {
+
+    lls_slt_service_id_group_id_cache_t* lls_slt_service_id_group_id_cache = lls_slt_monitor_find_or_create_lls_slt_service_id_group_id_cache_from_lls_group_id(lls_slt_monitor, lls_group_id);
+
+    lls_slt_service_id_group_id_cache_add_atsc3_lls_slt_service_cache(lls_slt_service_id_group_id_cache, atsc3_lls_slt_service);
+
+    return atsc3_lls_slt_service;
+
+}
+
+
+/*  lls_slt_monitor_find_lls_slt_service_id_group_id_cache_entry
+ *
+ *  find a LLS SLT service by service_id from lls_slt_monitor cached entries
+ *
+ *  note: we walk thru all group_id entries first, then find matching sls service_id
+ */
+atsc3_lls_slt_service_t* lls_slt_monitor_find_lls_slt_service_id_group_id_cache_entry(lls_slt_monitor_t* lls_slt_monitor, uint16_t service_id) {
+    lls_slt_service_id_group_id_cache_t* lls_slt_service_id_group_id_cache = NULL;
+    atsc3_lls_slt_service_t* atsc3_lls_slt_service = NULL;
+
+    for(int i=0; i < lls_slt_monitor->lls_slt_service_id_group_id_cache_v.count; i++) {
+        lls_slt_service_id_group_id_cache = lls_slt_monitor->lls_slt_service_id_group_id_cache_v.data[i];
+        for(int j=0; j < lls_slt_service_id_group_id_cache->atsc3_lls_slt_service_cache_v.count; j++) {
+            atsc3_lls_slt_service = lls_slt_service_id_group_id_cache->atsc3_lls_slt_service_cache_v.data[j];
+            if(atsc3_lls_slt_service->service_id == service_id) {
+                return atsc3_lls_slt_service;
+            }
+        }
+    }
+    return NULL;
+}
 
 
 
--- src/atsc3_phy_mmt_player_bridge.h
+++ src/atsc3_phy_mmt_player_bridge.h
@@ -7,10 +7,18 @@
 
 #include "../jni/At3DrvIntf.h"
 #include "atsc3_utils.h"
+#include "atsc3_lls_types.h"
 
 
 void atsc3_phy_mmt_player_bridge_init(At3DrvIntf* At3DrvIntf_ptr);
 
 void atsc3_phy_mmt_player_bridge_process_packet_phy(block_t* packet);
+
+//change SLT service and wire up a single montior
+atsc3_lls_slt_service_t* atsc3_phy_mmt_player_bridge_set_single_monitor_a331_service_id(int service_id);
+
+//todo:
+//atsc3_lls_slt_service_t* atsc3_phy_mmt_player_bridge_add_monitor_a331_service_id(int service_id);
+//atsc3_lls_slt_service_t* atsc3_phy_mmt_player_bridge_remove_monitor_a331_service_id(int service_id);
 
 #endif //AT3DRV_ANDROID_2_26_190826_ATSC3_PHY_MMT_PLAYER_BRIDGE_H
