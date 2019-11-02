d8e2f354f29e3376f24c44419bc1367b03c27ee8
Jason Justman <jjustman@ngbp.org>
2019-11-02 02:31:30 -0700

    memory cleanup for ROUTE mbms processing

--- src/atsc3_route_sls_processor.c
+++ src/atsc3_route_sls_processor.c
@@ -11,18 +11,26 @@
 
 
 void atsc3_route_sls_process_from_alc_packet_and_file(udp_flow_t* udp_flow, alc_packet_t* alc_packet, lls_sls_alc_monitor_t* lls_sls_alc_monitor) {
-
+    char* file_name = NULL;
+    char* mbms_toi_filename = NULL;
+    
+    FILE *fp = NULL;
+    FILE *fp_mbms = NULL;
+    
+    xml_document_t* fdt_xml = NULL;
+    atsc3_sls_metadata_fragments_t* atsc3_sls_metadata_fragments = NULL;
+    
 	//check if our toi == 0, if so, reprocess our sls fdt in preperation for an upcoming actual mbms emission
 	_ATSC3_ROUTE_SLS_PROCESSOR_INFO("alc_packet tsi/toi:%u/%u", alc_packet->def_lct_hdr->tsi, alc_packet->def_lct_hdr->toi);
 
 	if(alc_packet->def_lct_hdr->toi == 0) {
 
-	    char* file_name = alc_packet_dump_to_object_get_temporary_filename(udp_flow, alc_packet);
+	    file_name = alc_packet_dump_to_object_get_temporary_filename(udp_flow, alc_packet);
 
-		FILE *fp = fopen(file_name, "r");
+		fp = fopen(file_name, "r");
 		if(!fp) {
 			_ATSC3_ROUTE_SLS_PROCESSOR_ERROR("alc_packet tsi/toi:0/0 filename: %s is null!", file_name);
-			return;
+			goto cleanup;
 		}
 
 		xml_document_t* fdt_xml = xml_open_document(fp);
@@ -30,7 +38,7 @@ void atsc3_route_sls_process_from_alc_packet_and_file(udp_flow_t* udp_flow, alc_
 			atsc3_fdt_instance_t* atsc3_fdt_instance = atsc3_fdt_instance_parse_from_xml_document(fdt_xml);
 			if(!atsc3_fdt_instance) {
 				_ATSC3_ROUTE_SLS_PROCESSOR_ERROR("alc_packet tsi/toi:0/0 instance is null!");
-				return;
+				goto cleanup;
 			}
 			atsc3_fdt_instance_dump(atsc3_fdt_instance);
 			if(lls_sls_alc_monitor) {
@@ -44,32 +52,61 @@ void atsc3_route_sls_process_from_alc_packet_and_file(udp_flow_t* udp_flow, alc_
 			uint32_t* mbms_toi = atsc3_mbms_envelope_find_toi_from_fdt(lls_sls_alc_monitor->atsc3_fdt_instance);
 			if(!mbms_toi) {
 				_ATSC3_ROUTE_SLS_PROCESSOR_ERROR("Unable to find MBMS TOI");
-				return;
+				goto cleanup;
 			}
 
-		    char* mbms_toi_filename = alc_packet_dump_to_object_get_filename_tsi_toi(udp_flow, 0, *mbms_toi);
+            mbms_toi_filename = alc_packet_dump_to_object_get_filename_tsi_toi(udp_flow, 0, *mbms_toi);
 
-			FILE *fp_mbms= fopen(mbms_toi_filename, "r");
+			fp_mbms = fopen(mbms_toi_filename, "r");
 			if(!fp_mbms) {
 				_ATSC3_ROUTE_SLS_PROCESSOR_ERROR("alc_packet tsi/toi:0/%u filename: %s is null!", *mbms_toi, mbms_toi_filename);
-				return;
+				goto cleanup;
 			}
 
-			atsc3_sls_metadata_fragments_t* atsc3_sls_metadata_fragments = atsc3_mbms_envelope_to_sls_metadata_fragments_parse_from_fdt_fp(fp_mbms);
+			atsc3_sls_metadata_fragments = atsc3_mbms_envelope_to_sls_metadata_fragments_parse_from_fdt_fp(fp_mbms);
 
 			if(atsc3_sls_metadata_fragments) {
-
 				if(atsc3_sls_metadata_fragments->atsc3_route_s_tsid) {
 					//update our audio and video tsi and init
 					lls_sls_alc_update_tsi_toi_from_route_s_tsid(lls_sls_alc_monitor, atsc3_sls_metadata_fragments->atsc3_route_s_tsid);
 				}
-
+                if(lls_sls_alc_monitor->atsc3_sls_metadata_fragments) {
+                    //TODO: invoke any chained destructors as needed
+                    free(lls_sls_alc_monitor->atsc3_sls_metadata_fragments);
+                }
 				lls_sls_alc_monitor->atsc3_sls_metadata_fragments = atsc3_sls_metadata_fragments;
 			}
 		} else {
 			_ATSC3_ROUTE_SLS_PROCESSOR_ERROR("No pending atsc3_fdt_instance to process in TSI:0");
-			return;
+            goto cleanup;
 
 		}
 	}
+    
+cleanup:
+    if(file_name) {
+        free(file_name);
+        file_name = NULL;
+    }
+    
+    if(mbms_toi_filename) {
+        free(mbms_toi_filename);
+        mbms_toi_filename = NULL;
+    }
+    
+    if(fp) {
+        fclose(fp);
+        fp = NULL;
+    }
+    if(fp_mbms) {
+        fclose(fp_mbms);
+        fp_mbms = NULL;
+    }
+    
+    if(fdt_xml) {
+        xml_document_free(fdt_xml, true);
+    }
+
+    //mbms_toi is a pointer to atsc3_fdt_intstance alloc, so don't try and free it
+    
 }
