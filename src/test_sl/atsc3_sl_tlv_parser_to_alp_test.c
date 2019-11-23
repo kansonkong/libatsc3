6de63313f795c679eb999ad7a298c760501c692b
Jason Justman <jjustman@ngbp.org>
2019-11-23 07:54:30 -0800

    fixing block parsing

--- src/test_sl/atsc3_sl_tlv_parser_to_alp_test.c
+++ src/test_sl/atsc3_sl_tlv_parser_to_alp_test.c
@@ -53,6 +53,8 @@ int main(int argc, char* argv[] ) {
 
     if(atsc3_sl_tlv_fp) {
         while(!feof(atsc3_sl_tlv_fp)) {
+            _ATSC3_SL_TLV_PARSER_TO_ALP_TEST_DEBUG("fread: position: %ld", ftell(atsc3_sl_tlv_fp));
+
         	bytes_read = fread(buf, 1, 65535, atsc3_sl_tlv_fp);
         	if(bytes_read > 0) {
         		block_Write(atsc3_sl_tlv_block, buf, bytes_read);
@@ -73,13 +75,21 @@ int main(int argc, char* argv[] ) {
         		if(atsc3_sl_tlv_payload && !atsc3_sl_tlv_payload->alp_payload_complete && atsc3_sl_tlv_block->i_pos > atsc3_sl_tlv_payload->sl_tlv_total_parsed_payload_size) {
         			//accumulate from our last starting point and continue iterating over replay test filename
         			atsc3_sl_tlv_block->i_pos -= atsc3_sl_tlv_payload->sl_tlv_total_parsed_payload_size;
+        		}
 
+        		if(atsc3_sl_tlv_block->p_size > atsc3_sl_tlv_block->i_pos) {
+        			//truncate our current block_t starting at i_pos, and then read next i/o block
         			block_t* temp_sl_tlv_block = block_Duplicate_from_position(atsc3_sl_tlv_block);
-        			block_Destroy(&atsc3_sl_tlv_block);
-        			atsc3_sl_tlv_block = temp_sl_tlv_block;
+					block_Destroy(&atsc3_sl_tlv_block);
+					atsc3_sl_tlv_block = temp_sl_tlv_block;
+					block_Seek(atsc3_sl_tlv_block, atsc3_sl_tlv_block->p_size);
+        		} else {
+                    _ATSC3_SL_TLV_PARSER_TO_ALP_TEST_DEBUG("atsc3_sl_tlv_block: positioning mismatch: i_pos: %d, p_size: %d", atsc3_sl_tlv_block->i_pos, atsc3_sl_tlv_block->p_size);
+
+        			//jjustman: 2019-11-23: todo: try seek for our magic packet here
+        			block_Rewind(atsc3_sl_tlv_block);
         		}
         	}
-
         }
     }
 
