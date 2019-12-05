de66453d9f14a5568f62f11955db263db69859c3
Jason Justman <jjustman@ngbp.org>
2019-12-05 04:18:26 -0800

    setting stltp alp reflector back to plp0 and disabling auto-start with atsc3_listener_metrics_ncurses_httpd_isobmff - NOTE: TODO: jjustman-2019-12-05: route re-con playback is 'kinda-broken' due to proper ROUTE-ALC file naming

---
+++ xcode/libatsc3/atsc3_hevc_hls_test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  atsc3_hevc_hls_test
+//
+//  Created by Jason Justman on 12/5/19.
+//  Copyright © 2019 Jason Justman. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
