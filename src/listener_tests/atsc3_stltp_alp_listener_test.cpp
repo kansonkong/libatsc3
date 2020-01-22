00fbd6cb9122fc948f34b17589d703a78fc4838f
jjustman <jjustman@ngbp.org>
2020-01-22 08:48:49 -0800

    cleanup formatting

--- src/listener_tests/atsc3_stltp_alp_listener_test.cpp
+++ src/listener_tests/atsc3_stltp_alp_listener_test.cpp
@@ -55,29 +55,28 @@ void process_packet(u_char *user, const struct pcap_pkthdr *pkthdr, const u_char
 			if(atsc3_stltp_tunnel_packet_processed->atsc3_stltp_preamble_packet_v.count) {
 				__INFO(">>>stltp atsc3_stltp_preamble_packet packet complete: count: %u",  atsc3_stltp_tunnel_packet_processed->atsc3_stltp_preamble_packet_v.count);
 				for(int i=0; i < atsc3_stltp_tunnel_packet_processed->atsc3_stltp_preamble_packet_v.count; i++) {
-                    atsc3_stltp_preamble_packet_t* atsc3_stltp_preamble_packet = atsc3_stltp_tunnel_packet_processed->atsc3_stltp_preamble_packet_v.data[i];
-                    atsc3_preamble_packet_t* atsc3_preamble_packet = atsc3_stltp_parse_preamble_packet(atsc3_stltp_preamble_packet);
-                    if(!atsc3_preamble_packet) {
-                        __WARN("atsc3_preamble_packet is NULL for i: %d", i);
-                    } else {
-                        atsc3_preamble_packet_dump(atsc3_preamble_packet);
-                    }
+					atsc3_stltp_preamble_packet_t* atsc3_stltp_preamble_packet = atsc3_stltp_tunnel_packet_processed->atsc3_stltp_preamble_packet_v.data[i];
+					atsc3_preamble_packet_t* atsc3_preamble_packet = atsc3_stltp_parse_preamble_packet(atsc3_stltp_preamble_packet);
+					if(!atsc3_preamble_packet) {
+						__WARN("atsc3_preamble_packet is NULL for i: %d", i);
+					} else {
+                    	atsc3_preamble_packet_dump(atsc3_preamble_packet);
+					}
 				}
 			}
             
-            if(atsc3_stltp_tunnel_packet_processed->atsc3_stltp_timing_management_packet_v.count) {
-                 __INFO(">>>stltp atsc3_stltp_timing_management_packet packet complete: count: %u",  atsc3_stltp_tunnel_packet_processed->atsc3_stltp_timing_management_packet_v.count);
-                 for(int i=0; i < atsc3_stltp_tunnel_packet_processed->atsc3_stltp_timing_management_packet_v.count; i++) {
-                     atsc3_stltp_timing_management_packet_t* atsc3_stltp_timing_management_packet = atsc3_stltp_tunnel_packet_processed->atsc3_stltp_timing_management_packet_v.data[i];
-                     atsc3_timing_management_packet_t* atsc3_timing_management_packet = atsc3_stltp_parse_timing_management_packet(atsc3_stltp_timing_management_packet);
-                     if(!atsc3_timing_management_packet) {
-                         __WARN("atsc3_timing_management_packet is NULL for i: %d", i);
-                     } else {
-                         atsc3_timing_management_packet_dump(atsc3_timing_management_packet);
-                     }
-                 }
-             }
-            
+			if(atsc3_stltp_tunnel_packet_processed->atsc3_stltp_timing_management_packet_v.count) {
+            	__INFO(">>>stltp atsc3_stltp_timing_management_packet packet complete: count: %u",  atsc3_stltp_tunnel_packet_processed->atsc3_stltp_timing_management_packet_v.count);
+            	for(int i=0; i < atsc3_stltp_tunnel_packet_processed->atsc3_stltp_timing_management_packet_v.count; i++) {
+            		atsc3_stltp_timing_management_packet_t* atsc3_stltp_timing_management_packet = atsc3_stltp_tunnel_packet_processed->atsc3_stltp_timing_management_packet_v.data[i];
+            		atsc3_timing_management_packet_t* atsc3_timing_management_packet = atsc3_stltp_parse_timing_management_packet(atsc3_stltp_timing_management_packet);
+            		if(!atsc3_timing_management_packet) {
+            			__WARN("atsc3_timing_management_packet is NULL for i: %d", i);
+            		} else {
+                    	 atsc3_timing_management_packet_dump(atsc3_timing_management_packet);
+            		}
+            	}
+			}
             atsc3_stltp_tunnel_packet_clear_completed_inner_packets(atsc3_stltp_tunnel_packet_processed);
 		} else {
             __ERROR("error processing packet: %p, size: %u",  ip_udp_rtp_packet, ip_udp_rtp_packet->data->p_size);
