d1aaa698685986e036aa7469d9649f5eea59550e
jjustman <jjustman@ngbp.org>
2020-03-25 22:23:44 -0700

    fixing multi-ALC flows with alc_listener_mde_writer test

--- src/atsc3_lls_alc_utils.h
+++ src/atsc3_lls_alc_utils.h
@@ -32,8 +32,10 @@ lls_sls_alc_monitor_t* lls_sls_alc_monitor_create(void);
 lls_sls_alc_session_t* lls_slt_alc_session_create(atsc3_lls_slt_service_t* atsc3_lls_slt_service);
 lls_sls_alc_session_t* lls_slt_alc_session_find_or_create(lls_slt_monitor_t* lls_slt_monitor, atsc3_lls_slt_service_t* atsc3_lls_slt_service);
 
-lls_sls_alc_session_t* lls_slt_alc_session_find(lls_slt_monitor_t* lls_slt_monitor, atsc3_lls_slt_service_t* atsc3_lls_slt_service);
+lls_sls_alc_monitor_t* atsc3_lls_sls_alc_monitor_find_from_udp_packet(lls_slt_monitor_t* lls_slt_monitor, uint32_t src_ip_addr, uint32_t dst_ip_addr, uint16_t dst_port);
 
+//jjustman-2020-03-25: TODO - deprecate single lls_sls_alc_session resolution, instead use lls_sls_alc_montitor_t for alc session extraction
+lls_sls_alc_session_t* lls_slt_alc_session_find(lls_slt_monitor_t* lls_slt_monitor, atsc3_lls_slt_service_t* atsc3_lls_slt_service);
 lls_sls_alc_session_t* lls_slt_alc_session_find_from_udp_packet(lls_slt_monitor_t* lls_slt_monitor, uint32_t src_ip_addr, uint32_t dst_ip_addr, uint16_t dst_port);
 lls_sls_alc_session_t* lls_slt_alc_session_find_from_service_id(lls_slt_monitor_t* lls_slt_monitor, uint16_t service_id);
 
