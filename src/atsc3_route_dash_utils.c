db421e0b9d7d5183dda6659ca2d3e75ac8d2f1c8
Jason Justman <jjustman@ngbp.org>
2020-07-13 19:51:33 -0700

    checkpoint for pcre2 support for route dash representation flow patching from s-tsid w/ startNumber

---
+++ src/atsc3_route_dash_utils.c
@@ -0,0 +1,14 @@
+/*
+ * atsc3_route_dash_utils.c
+ *
+ *  Created on: Jul 6, 2020
+ *      Author: jjustman
+ */
+
+#include "atsc3_route_dash_utils.h"
+
+int _ROUTE_DASH_UTILS_INFO_ENABLED = 1;
+int _ROUTE_DASH_UTILS_DEBUG_ENABLED = 0;
+int _ROUTE_DASH_UTILS_TRACE_ENABLED = 0;
+
+
---
+++ src/atsc3_route_dash_utils.h
@@ -0,0 +1,41 @@
+/*
+ * atsc3_route_dash_utils.h
+ *
+ *  Created on: Jul 13, 2020
+ *      Author: jjustman
+ */
+
+
+
+#ifndef ATSC3_ROUTE_DASH_UTILS_H
+#define ATSC3_ROUTE_DASH_UTILS_H
+
+#if defined (__cplusplus)
+extern "C" {
+#endif
+
+#include "atsc3_utils.h"
+#include "atsc3_logging_externs.h"
+#include "atsc3_pcre2_regex_utils.h"
+
+#define ATSC3_ROUTE_DASH_MPD_REPRESENTATION_ID_SEGMENT_TEMPLATE_START_NUMBER_REGEX_PATTERN "(<Representation.*?id=\"(.*?)\".*?>.*?<SegmentTemplate.*?startNumber=\"(.*?)\".*?<\\/Representation>)"
+#define ATSC3_ROUTE_DASH_MPD_REPRESENTATION_ID_SEGMENT_TEMPLATE_START_NUMBER_REGEX_FLAGS "msg"
+
+
+
+
+
+
+
+
+#define __ROUTE_DASH_UTILS_ERROR(...) __LIBATSC3_TIMESTAMP_ERROR(__VA_ARGS__);
+#define __ROUTE_DASH_UTILS_WARN(...)  __LIBATSC3_TIMESTAMP_WARN(__VA_ARGS__);
+#define __ROUTE_DASH_UTILS_INFO(...)  if(_ROUTE_DASH_UTILS_INFO_ENABLED)  { __LIBATSC3_TIMESTAMP_INFO(__VA_ARGS__); }
+#define __ROUTE_DASH_UTILS_DEBUG(...) if(_ROUTE_DASH_UTILS_DEBUG_ENABLED) { __LIBATSC3_TIMESTAMP_DEBUG(__VA_ARGS__); }
+#define __ROUTE_DASH_UTILS_TRACE(...) if(_ROUTE_DASH_UTILS_TRACE_ENABLED) { __LIBATSC3_TIMESTAMP_DEBUG(__VA_ARGS__); }
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* ATSC3_PCRE2_REGEX_UTILS_H_ */
