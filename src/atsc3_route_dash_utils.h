c6d8e2285d847a7c24470426ad9eebc2e2d2ed49
Jason Justman <jjustman@ngbp.org>
2020-07-14 06:49:32 -0700

    checkpoint: mpd route dash matching with s-tsid working, next step to patch mpd based upon match_start and match_end replacement

--- src/atsc3_route_dash_utils.h
+++ src/atsc3_route_dash_utils.h
@@ -18,11 +18,18 @@ extern "C" {
 #include "atsc3_logging_externs.h"
 #include "atsc3_pcre2_regex_utils.h"
 
+#include "atsc3_route_s_tsid.h"
+#include "atsc3_lls_types.h"
+
 #define ATSC3_ROUTE_DASH_MPD_REPRESENTATION_ID_SEGMENT_TEMPLATE_START_NUMBER_REGEX_PATTERN "(<Representation.*?id=\"(.*?)\".*?>.*?<SegmentTemplate.*?startNumber=\"(.*?)\".*?<\\/Representation>)"
 #define ATSC3_ROUTE_DASH_MPD_REPRESENTATION_ID_SEGMENT_TEMPLATE_START_NUMBER_REGEX_FLAGS "msg"
+#define ATSC3_ROUTE_DASH_MPD_REPRESENTATION_ID_SEGMENT_TEMPLATE_START_NUMBER_REGEX_CAPTURE_REFERENCE 2
+
 
 
 //atsc3_pcre2_regex_match_capture_vector_t* atsc3_pcre2_regex_match_capture_vector
+
+void atsc3_route_dash_find_matching_s_tsid_representations_from_mpd_pcre2_regex_matches(atsc3_pcre2_regex_match_capture_vector_t* atsc3_pcre2_regex_match_capture_vector, atsc3_sls_alc_flow_v* atsc3_sls_alc_flow);
 
 
 
