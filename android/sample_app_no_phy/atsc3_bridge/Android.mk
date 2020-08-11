9651fd23a3faa52542b107760f4308d673f45ef9
Jason Justman <jjustman@ngbp.org>
2020-08-10 17:22:51 -0700

    WIP - almost compiling properly with prefab..!

---
+++ android/sample_app_no_phy/atsc3_bridge/Android.mk
@@ -0,0 +1,62 @@
+# Android.mk for libatsc3 Android sample app
+#
+#
+# jjustman@ngbp.org - for libatsc3 inclusion 2019-09-28
+
+# global pathing
+
+MY_LOCAL_PATH := $(call my-dir)
+LOCAL_PATH := $(call my-dir)
+MY_CUR_PATH := $(LOCAL_PATH)
+
+#NDK_MODULE_PATH := $(LOCAL_PATH)../atsc3_core/
+#
+#$(call import-add-path,$(NDK_GRADLE_INJECTED_IMPORT_PATH))
+# $(call import-module,atsc3_core)
+$(call import-module,prefab/atsc3_core)
+
+# ---------------------------
+# libatsc3_bridge jni interface
+
+LOCAL_PATH := $(MY_LOCAL_PATH)
+include $(CLEAR_VARS)
+# LOCAL_ALLOW_UNDEFINED_SYMBOLS=true
+
+LOCAL_MODULE := atsc3_bridge
+
+LIBATSC3COREBRIDGECPP := \
+    $(wildcard $(LOCAL_PATH)/src/cpp/*.cpp)
+
+LIBATSC3JNIBRIDGECPP := \
+    $(wildcard $(LOCAL_PATH)/src/jni/*.cpp)
+
+# jjustman-2020-08-10 - temporary - refactor this out...
+LOCAL_SRC_FILES += \
+    $(LIBATSC3COREBRIDGECPP:$(LOCAL_PATH)/%=%)  \
+    $(LIBATSC3JNIBRIDGECPP:$(LOCAL_PATH)/%=%)
+
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/src/cpp
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/src/jni
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../../../src/
+
+# jjustman-2020-08-10 - hack-ish...
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../atsc3_core/libpcre/include
+
+LOCAL_CFLAGS += -g -fpack-struct=8 -fPIC  \
+                -D__DISABLE_LIBPCAP__ -D__DISABLE_ISOBMFF_LINKAGE__ -D__DISABLE_NCURSES__ \
+                -D__MOCK_PCAP_REPLAY__ -D__LIBATSC3_ANDROID__
+
+LOCAL_LDLIBS := -ldl -llog -landroid -lz
+
+# $(call import-module, $(LOCAL_PATH)/../libatsc3_core)
+ LOCAL_SHARED_LIBRARIES := atsc3_core
+
+# include ${ANDROID_GRADLE_NATIVE_BUNDLE_PLUGIN_MK}
+include $(BUILD_SHARED_LIBRARY)
+
+#    $(call import-add-path,$(NDK_GRADLE_INJECTED_IMPORT_PATH))
+
+# $(call import-module,prefab/atsc3_core)
+
+
+
