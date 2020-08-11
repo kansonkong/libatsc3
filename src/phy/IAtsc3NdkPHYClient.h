2e0d411ad8bfc017ea714de28b8d1aaa4b0f2266
Jason Justman <jjustman@ngbp.org>
2020-08-11 09:57:26 -0700

    WIP: adding PcapSTLTPVirtualPHY and test(s)
    PcapSTLTPVirtualPHYTest.:  55:DEBUG:1597164962.0645:pcap_file_pos:
    124965484, pcap_file_len: 142998594
    PcapSTLTPVirtualPHYTest.:  55:DEBUG:1597164963.0651:pcap_file_pos:
    127867668, pcap_file_len: 142998594
    PcapSTLTPVirtualPHYTest.:  55:DEBUG:1597164964.0670:pcap_file_pos:
    130742266, pcap_file_len: 142998594
    PcapSTLTPVirtualPHY.cpp : 212:INFO
    :1597164964.0952:PcapSTLTPVirtualPHY::RunPcapThreadParser - pushing to
    atsc3_core_service_bridge_process_packet_phy: count: 90001, len was:
    1414, new payload: 0x603000722920 (0x45 0x00), len: 1400
    PcapSTLTPVirtualPHYTest.:  55:DEBUG:1597164965.0675:pcap_file_pos:
    133676398, pcap_file_len: 142998594
    PcapSTLTPVirtualPHYTest.:  26:DEBUG:1597164965.3609:PLP: 0, packet
    number: 13001, packet: 0x603000251ce0, len: 1448
    atsc3_stltp_parser.c    : 270:WARN
    :1597164965.6255:atsc3_stltp_tunnel_packet_extract_inner_from_outer_packet:
    final ip_udp_rtp_packet_outer does not match pos: 87, size: 1400
    PcapSTLTPVirtualPHYTest.:  55:DEBUG:1597164966.0713:pcap_file_pos:
    136584398, pcap_file_len: 142998594
    PcapSTLTPVirtualPHYTest.:  55:DEBUG:1597164967.0718:pcap_file_pos:
    139505484, pcap_file_len: 142998594
    PcapSTLTPVirtualPHYTest.:  55:DEBUG:1597164968.0726:pcap_file_pos:
    142409122, pcap_file_len: 142998594
    PcapSTLTPVirtualPHY.cpp : 244:INFO
    :1597164968.2765:PcapSTLTPVirtualPHY::RunPcapThreadParser - unwinding
    thread, pcapThreadShouldRun is false
    PcapSTLTPVirtualPHYTest.:  55:DEBUG:1597164969.0730:pcap_file_pos:
    142999446, pcap_file_len: 142998594
    PcapSTLTPVirtualPHYTest.:  71:INFO :1597164969.0730:...completed, start
    time: 1597164918.998, end time: 1597164969.073, duration: 50.075,

--- src/phy/IAtsc3NdkPHYClient.h
+++ src/phy/IAtsc3NdkPHYClient.h
@@ -4,15 +4,17 @@
 #define LIBATSC3_IATSC3NDKPHYCLIENT_H
 
 
-typedef void(*atsc3_rx_udp_packet_process_callback_f)(block_t* block);
+//will dispatch plp_num as 0 if the (virtual/demuxed) phy does not have support to disambiguate (e.g. no LMT reference)
+
+typedef void(*atsc3_phy_rx_udp_packet_process_callback_f)(uint8_t plp_num, block_t* block);
 
 class IAtsc3NdkPHYClient {
 
 	public:
 		virtual int Init() = 0;
 
-		virtual void SetRxUdpPacketProcessCallback(atsc3_rx_udp_packet_process_callback_f atsc3_rx_udp_packet_process_callback) {
-			this->atsc3_rx_udp_packet_process_callback = atsc3_rx_udp_packet_process_callback;
+		virtual void SetRxUdpPacketProcessCallback(atsc3_phy_rx_udp_packet_process_callback_f atsc3_phy_rx_udp_packet_process_callback) {
+			this->atsc3_phy_rx_udp_packet_process_callback = atsc3_phy_rx_udp_packet_process_callback;
 		}
 
 		virtual int Open(int fd, int bus, int addr) = 0;
@@ -22,15 +24,15 @@ class IAtsc3NdkPHYClient {
 
 		virtual ~IAtsc3NdkPHYClient() {};
 
-    /* jjustman-2020-08-10
-     * additional methods to impl?
-     *
-     *   int TuneMultiplePLP(int freqKhz, vector<int> plpIds);
-    int ListenPLP1(int plp1); //by default, we will always listen to PLP0, append additional PLP for listening
-     */
+		/* jjustman-2020-08-10
+		 * additional methods to impl?
+		 *
+		 *   int TuneMultiplePLP(int freqKhz, vector<int> plpIds);
+		int ListenPLP1(int plp1); //by default, we will always listen to PLP0, append additional PLP for listening
+		 */
 
 	protected:
-		atsc3_rx_udp_packet_process_callback_f atsc3_rx_udp_packet_process_callback;
+		atsc3_phy_rx_udp_packet_process_callback_f atsc3_phy_rx_udp_packet_process_callback;
 };
 
 
