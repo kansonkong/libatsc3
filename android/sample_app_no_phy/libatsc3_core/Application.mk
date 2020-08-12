152764970bf196a3195649c17967d057cbd1b9a6
Jason Justman <jjustman@ngbp.org>
2020-08-12 08:24:12 -0700

    WIP - ndk complication passes with SRT built-in - avoid excess linkages for stdc++ so

--- android/sample_app_no_phy/libatsc3_core/Application.mk
+++ android/sample_app_no_phy/libatsc3_core/Application.mk
@@ -1,16 +1,15 @@
 #
 # Application.mk
 #
-# note that APP_ABI is overridden by build.gradle setting.
-#
+# jjustman-2020-08-12- remove individual linkage to APP_STL := c++_shared as android studio 4.0 complains about
+# 3 files found with path 'lib/arm64-v8a/libc++_shared.so'.
+
+APP_STL := c++_shared
+
 APP_DEBUG := true
 APP_STRIP_MODE := "none"
 
-#APP_STL := gnustl_shared
-APP_STL := c++_shared
-
 APP_ABI := all
-# APP_ABI := arm64-v8a
 
 APP_CPPFLAGS += -std=c++11 -D_ANDROID
 
--- android/sample_app_no_phy/libatsc3_phy_virtual/Application.mk
+++ android/sample_app_no_phy/libatsc3_phy_virtual/Application.mk
@@ -2,12 +2,11 @@
 # Application.mk
 #
 # note that APP_ABI is overridden by build.gradle setting.
-#
+# jjustman-2020-08-12- remove individual linkage to APP_STL := c++_shared as android studio 4.0 complains about
+# 3 files found with path 'lib/arm64-v8a/libc++_shared.so'.
+
 APP_DEBUG := true
 APP_STRIP_MODE := "none"
-
-#APP_STL := gnustl_shared
-APP_STL := c++_shared
 
 APP_ABI := all
 # APP_ABI := arm64-v8a
