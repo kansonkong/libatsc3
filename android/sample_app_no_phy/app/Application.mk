1bae04f33383e2892c76148cec0144e14e7ef967
Jason Justman <jjustman@ngbp.org>
2020-08-12 08:24:12 -0700

    WIP - ndk complication passes with SRT built-in - avoid excess linkages for stdc++ so

--- android/sample_app_no_phy/app/Application.mk
+++ android/sample_app_no_phy/app/Application.mk
@@ -3,12 +3,9 @@
 #
 # note that APP_ABI is overridden by build.gradle setting.
 #
-
-#APP_STL := gnustl_shared
-APP_STL := c++_shared
+APP_DEBUG := true
 
 APP_ABI := all
-# APP_ABI := arm64-v8a
 
-APP_CPPFLAGS += -std=c++11 -D_ANDROID
+APP_CPPFLAGS += -std=c++11 -D_ANDROID -fexceptions
 
