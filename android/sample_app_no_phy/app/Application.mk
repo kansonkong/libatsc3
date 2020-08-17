fafcaa39d9fe347cb89dbe65fe31f6adf77edea2
Jason Justman <jjustman@ngbp.org>
2020-08-17 12:43:14 -0700

    WIP - checkpoint

--- android/sample_app_no_phy/app/Application.mk
+++ android/sample_app_no_phy/app/Application.mk
@@ -4,8 +4,14 @@
 # note that APP_ABI is overridden by build.gradle setting.
 #
 APP_DEBUG := true
+APP_STRIP_MODE := "none"
 
 APP_ABI := all
 
+APP_STL := c++_shared
 APP_CPPFLAGS += -std=c++11 -fexceptions -D_ANDROID
+
+#only the main app build will get the c++ shared .so
+
+
 
