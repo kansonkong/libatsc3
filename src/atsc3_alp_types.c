90e2cb355f48572bc91328ce15e5d44c9bd6b282
Jason Justman <jjustman@ngbp.org>
2020-09-02 05:43:34 -0700

    adding missing bootstrap timing data when cloning atsc3_alp_packet

--- src/atsc3_alp_types.c
+++ src/atsc3_alp_types.c
@@ -27,6 +27,7 @@ ATSC3_VECTOR_BUILDER_METHODS_ITEM_FREE(atsc3_link_mapping_table_multicast);
 
 atsc3_alp_packet_t* atsc3_alp_packet_clone(atsc3_alp_packet_t* atsc3_alp_packet) {
     atsc3_alp_packet_t* atsc3_alp_packet_new = calloc(1, sizeof(atsc3_alp_packet_t));
+    atsc3_alp_packet_new->bootstrap_timing_data_timestamp_short_reference = atsc3_alp_packet->bootstrap_timing_data_timestamp_short_reference;
     atsc3_alp_packet_new->plp_num = atsc3_alp_packet->plp_num;
 
     memcpy(&atsc3_alp_packet_new->alp_packet_header, &atsc3_alp_packet->alp_packet_header, sizeof(alp_packet_header_t));
