72248f58265dcf1fdbe62c60dcacf0455331265e
Jason Justman <jjustman@ngbp.org>
2020-11-17 20:43:59 -0800

    checkpoint for SLS validation with unit test driver

--- src/test/atsc3_smime_validation_test.c
+++ src/test/atsc3_smime_validation_test.c
@@ -18,23 +18,45 @@
 #define _ATSC3_SMIME_UTILS_TEST_INFO(...)    printf("%s:%d:INFO:",__FILE__,__LINE__);_ATSC3_UTILS_PRINTLN(__VA_ARGS__);
 #define _ATSC3_SMIME_UTILS_TEST_DEBUG(...)   printf("%s:%d:DEBUG:",__FILE__,__LINE__);_ATSC3_UTILS_PRINTLN(__VA_ARGS__);
 
-int parse_signed_sls(const char* filename) {
+int parse_signed_sls_with_test_certificate_noverify(const char* sls_fragment_filename, const char* signing_certificate_filename) {
 	int ret = 0;
 
-	atsc3_smime_entity_t* atsc3_smime_entity = atsc3_smime_entity_new_parse_from_file(filename);
+	atsc3_smime_entity_t* atsc3_smime_entity = atsc3_smime_entity_new_parse_from_file(sls_fragment_filename);
 	atsc3_smime_validation_context_t* atsc3_smime_validation_context = atsc3_smime_validation_context_new(atsc3_smime_entity);
 	
-	atsc3_smime_validate_from_context(atsc3_smime_validation_context);
+	atsc3_smime_validation_context_set_cms_noverify(atsc3_smime_validation_context, true);
+	atsc3_smime_validation_context_certificate_payload_parse_from_file(atsc3_smime_validation_context, signing_certificate_filename);
 	
-		
+	atsc3_smime_validation_context_t* atsc3_smime_validation_context_ret = atsc3_smime_validate_from_context(atsc3_smime_validation_context);
+	
+	if(!atsc3_smime_validation_context_ret && !atsc3_smime_validation_context->cms_signature_valid) {
+		_ATSC3_SMIME_UTILS_TEST_INFO("parse_signed_sls_with_test_certificate_noverify failed: from sls_fragment_filename: %s, signing_certificate_filename: %s failed!", sls_fragment_filename, signing_certificate_filename);
+		ret = -1;
+	} else if(!atsc3_smime_validation_context_ret || !atsc3_smime_validation_context->cms_signature_valid) {
+		_ATSC3_SMIME_UTILS_TEST_ERROR("parse_signed_sls_with_test_certificate_noverify RETURN mismatch (%p) with cms_signature_valid: (%d), from: sls_fragment_filename: %s, signing_certificate_filename: %s failed!",
+									  atsc3_smime_validation_context_ret,
+									  atsc3_smime_validation_context->cms_signature_valid,
+									  sls_fragment_filename, signing_certificate_filename);
+		ret = -31337;
+	} else if(atsc3_smime_validation_context_ret && atsc3_smime_validation_context->cms_signature_valid) {
+		_ATSC3_SMIME_UTILS_TEST_INFO("parse_signed_sls_with_test_certificate_noverify success: from: sls_fragment_filename: %s, signing_certificate_filename: %s, cms_verified_extracted_mime_entity_len: %d, cms_verified_extracted_mime_entity:\n%s",
+									 sls_fragment_filename, signing_certificate_filename,
+									 atsc3_smime_validation_context->atsc3_smime_entity->cms_verified_extracted_mime_entity->p_size,
+									 atsc3_smime_validation_context->atsc3_smime_entity->cms_verified_extracted_mime_entity->p_buffer);
+		ret = 0;
+	} else {
+		_ATSC3_SMIME_UTILS_TEST_ERROR("parse_signed_sls_with_test_certificate_noverify: ERROR!");
+		return -1000000;
+	}
 
 	return ret;
 }
 int main(int argc, char* argv[] ) {
 
 
-	 //parse_fdt("../test_data/xml_fdt/phx-fdt-0-0.xml");
-	 parse_signed_sls("testdata/2020-11-17-signed-sls/239.1.120.120.49152.0-458826");
+	//manual test:  ./openssl cms -verify -binary -in testdata/2020-11-17-signed-sls/239.1.120.120.49152.0-458826 -noverify -certfile testdata/2020-11-17-signed-sls/signal-signing-enensys-SMT.crt
+
+	 parse_signed_sls_with_test_certificate_noverify("testdata/2020-11-17-signed-sls/239.1.120.120.49152.0-458826", "testdata/2020-11-17-signed-sls/signal-signing-enensys-SMT.crt");
 	 return 0;
 }
 
