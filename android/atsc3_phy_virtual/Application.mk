e4d311d319599214fa073e3d531e2a1c3d3685b6
viktor.matiash <nrkpbntokxcdrsowhk7bskzpzep5g6hzgkl4f3yum6bmjq4usa2q>
2020-11-30 11:08:03 +0200

    fix: disable hwasan for all platforms except armeabi-v7a and enable publishing scripts.

--- android/atsc3_phy_virtual/Application.mk
+++ android/atsc3_phy_virtual/Application.mk
@@ -11,8 +11,10 @@ APP_ABI := arm64-v8a
 APP_CPPFLAGS += -std=c++11 -fexceptions -D_ANDROID
 
 # jjustman-2020-10-07 - hwasan is only available on android 10 or higher
+ifeq ($(ENABLE_HWASAN),armeabi-v7a)
 APP_CFLAGS := -fsanitize=hwaddress -fno-omit-frame-pointer -g -O0
 APP_LDFLAGS := -fsanitize=hwaddress
+endif
 
 # fall back to plain asan
 # ifeq ($(ENABLE_HWASAN),xxxx)
