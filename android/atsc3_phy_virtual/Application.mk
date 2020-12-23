e081d48c476cc7d192f2aac2ef9d254a29bc4b16
Jason Justman <jjustman@ngbp.org>
2020-12-23 00:53:59 -0800

    #8900 - refactoring for module (shared or prebuilt) usage and asan/hwasan linkage
    #8900 - refactoring for module (shared or prebuilt) usage and asan/hwasan linkage

--- android/atsc3_phy_virtual/Application.mk
+++ android/atsc3_phy_virtual/Application.mk
@@ -1,26 +1,19 @@
 #
 # Application.mk
 #
-
 APP_DEBUG := true
 APP_STRIP_MODE := "none"
 
 APP_STL := c++_shared
-
-# APP_ABI := arm64-v8a armeabi-v7a
-
 APP_CPPFLAGS += -std=c++11 -fexceptions -D_ANDROID
+LOCAL_ARM_MODE := arm
 
-# jjustman-2020-10-07 - hwasan is only available on android 10 or higher
-ifeq ($(ENABLE_HWASAN),armeabi-v7a)
-APP_CFLAGS := -fsanitize=hwaddress -fno-omit-frame-pointer -g -O0
-APP_LDFLAGS := -fsanitize=hwaddress
+ifeq ($(debugging_asan_enabled), true)
+$(info 'building atsc3_phy_virtual with debugging_asan_enabled' )
+	APP_CFLAGS += -fsanitize=address -fno-omit-frame-pointer -g -O0 -fno-optimize-sibling-calls -fsanitize-address-use-after-scope
+	APP_LDFLAGS += -fsanitize=address
+else ifeq ($(debugging_hwasan_enabled), true)
+$(info 'building atsc3_phy_virtual with debugging_hwasan_enabled' )
+	APP_CFLAGS += -fsanitize=hwaddress -fno-omit-frame-pointer  -g -O0 -fno-optimize-sibling-calls -fsanitize-address-use-after-scope
+	APP_LDFLAGS += -fsanitize=hwaddress
 endif
-
-# fall back to plain asan
-# ifeq ($(ENABLE_HWASAN),xxxx)
-# APP_CFLAGS := -fsanitize=address -fno-omit-frame-pointer -g -O1 -fno-optimize-sibling-calls
-# APP_LDFLAGS := -fsanitize=address
-# endif
-
-
