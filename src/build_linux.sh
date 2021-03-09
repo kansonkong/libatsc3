1d7d30418dca91e766a2bef836c8919100e2af6c
Jason Justman <jjustman@ngbp.org>
2021-03-09 12:19:29 -0800

    linux build fixups, include srt and deps

--- src/build_linux.sh
+++ src/build_linux.sh
@@ -11,5 +11,8 @@ cd ../libmicrohttpd && tar -xvzf libmicrohttpd-latest.tar.gz && cd libmicrohttpd
 # Compile Bento4
 cd ../../bento && mv lib lib-tmp && mkdir lib && mv lib-tmp/place_libap4.a_bento_file_here lib && mv lib-tmp/.gitignore lib && rm -Rf lib-tmp && cd lib && git clone https://github.com/axiomatic-systems/Bento4.git && cd Bento4 && mkdir cmakebuild && cd cmakebuild && cmake -DCMAKE_BUILD_TYPE=Debug .. && make && mv * ../..
 
+# Compile SRT
+cd ../srt && ./configure --prefix `pwd`/build && make && make install
+
 # Compile libatsc3
 cd ../../../../src && make all
