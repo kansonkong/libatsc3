b42fbca0bf27d4459fb97c7d54c1d02cb4cf5f31
viktor.matiash <nrkpbntokxcdrsowhk7bskzpzep5g6hzgkl4f3yum6bmjq4usa2q>
2021-03-16 13:53:08 +0200

    feat: optimize MMT playback initialization.

--- android/atsc3_bridge_media_mmt/src/jni/Atsc3RingBuffer.h
+++ android/atsc3_bridge_media_mmt/src/jni/Atsc3RingBuffer.h
@@ -27,9 +27,7 @@ public:
     Atsc3RingBuffer(uint8_t* buffer_ptr, uint32_t buffer_size, uint32_t page_size);
 
     void write(int8_t type, uint16_t service_id, uint16_t packet_id, uint32_t sample_number, uint64_t presentationUs, uint8_t* buffer, uint32_t bufferLen);
-
-    uint32_t getCurrentPosition();
-    uint32_t getCurrentPageNumber();
+    void rewind();
 };
 
 #endif //LIBATSC3_RING_BUFFER_H
