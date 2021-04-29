3d8250798e10f2a093e7ad56b69b8cb03a5ffb0e
Jason Justman <jjustman@sinclairdigital.com>
2021-04-29 07:46:43 -0700

    device dongle selection

--- android/atsc3_phy_saankhya/src/main/java/org/ngbp/libatsc3/middleware/android/phy/SaankhyaPHYAndroid.java
+++ android/atsc3_phy_saankhya/src/main/java/org/ngbp/libatsc3/middleware/android/phy/SaankhyaPHYAndroid.java
@@ -7,11 +7,12 @@ import java.util.List;
 public class SaankhyaPHYAndroid extends Atsc3NdkPHYSaankhyaStaticJniLoader  {
 
     //jjustman-2021-04-28 - <1204, 243> is the generic VID/PID for the FX3 bootloader
-    public static final int CYPRESS_VENDOR_ID = 1204;
+    public static final int CYPRESS_VENDOR_ID =     1204;
     public static final int FX3_PREBOOT_PRODUCT_ID = 243;
 
     //this PID is only re-enumerated after our FX3 preboot firmware has been loaded based upon DeviceTypeSelectionDialog disambiguation
-    public static final int KAILASH_PRODUCT_ID  = 240;
+    public static final int KAILASH_OR_YOGA_PRODUCT_ID = 240;
+    public static final String KAILASH_FIRMWARE_MFG_NAME_JJ = "JJ5ress";
 
     // The same values as in SaankhyaPHYAndroid.h
     public static final int DEVICE_TYPE_MARKONE     = 0;
@@ -20,7 +21,7 @@ public class SaankhyaPHYAndroid extends Atsc3NdkPHYSaankhyaStaticJniLoader  {
 
     static {
         Atsc3NdkPHYClientBase.AllRegisteredPHYImplementations.add(new USBVendorIDProductIDSupportedPHY(CYPRESS_VENDOR_ID, 243, "SL-FX3-Preboot", true, SaankhyaPHYAndroid.class));
-        Atsc3NdkPHYClientBase.AllRegisteredPHYImplementations.add(new USBVendorIDProductIDSupportedPHY(CYPRESS_VENDOR_ID, KAILASH_PRODUCT_ID, "SL-KAILASH", false, SaankhyaPHYAndroid.class));
+        Atsc3NdkPHYClientBase.AllRegisteredPHYImplementations.add(new USBVendorIDProductIDSupportedPHY(CYPRESS_VENDOR_ID, KAILASH_OR_YOGA_PRODUCT_ID, "SL-KAILASH", false, SaankhyaPHYAndroid.class));
         Log.w("SaankhyaPHYAndroid", String.format("static constructor, allRegisteredPHYImplementations is now %d elements: ",Atsc3NdkPHYClientBase.AllRegisteredPHYImplementations.size()));
 
     }
