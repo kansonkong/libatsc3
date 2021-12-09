76f4ecee75295be8c17a27a8c90ac3b9da443e2a
Jason Justman <jjustman@ngbp.org>
2021-12-10 00:57:25 +0530

    block_t prealloc and soft resize fixes

--- src/atsc3_utils.c
+++ src/atsc3_utils.c
@@ -621,10 +621,9 @@ block_t* block_Duplicate_from_ptr(uint8_t* data, uint32_t size) {
 
 #define __ATSC3_UTILS_BLOCK_RESIZE_DOUBLE_LIMIT__ 2048000
 //perform a soft allocation to
-//src->a_size * 2  where dest_size_min_required < 2M (__ATSC3_UTILS_BLOCK_RESIZE_DOUBLE_LIMIT__)
+//src->p_size * 2  where dest_size_min_required < 2M (__ATSC3_UTILS_BLOCK_RESIZE_DOUBLE_LIMIT__)
 block_t* block_Resize_Soft(block_t* dest, uint32_t dest_size_min_required) {
 	if(!__block_check_bounaries(__FUNCTION__, dest)) return NULL;
-	uint32_t dest_size_original = dest->p_size;
 
 	if(dest->_a_size >= dest_size_min_required && dest->i_pos < dest_size_min_required && dest->p_size < dest_size_min_required) {
 		dest->p_size = dest_size_min_required;
@@ -638,11 +637,36 @@ block_t* block_Resize_Soft(block_t* dest, uint32_t dest_size_min_required) {
 	}
 
 	block_Resize(dest, target_dest_size_min_required);
-	dest->p_size = dest_size_original;
+	dest->p_size = dest_size_min_required;
 
 	return dest;
 }
 
+
+#define __ATSC3_UTILS_BLOCK_PREALLOC_RESIZE_DOUBLE_LIMIT__ 8192000
+//perform a pre-allocation soft to
+//src->a_size * 2  where soft_alloc_size < 2M (__ATSC3_UTILS_BLOCK_RESIZE_DOUBLE_LIMIT__)
+//while keeping dest->p_size as original size
+block_t* block_Resize_Prealloc_Soft(block_t* dest, uint32_t soft_alloc_size) {
+	if(!__block_check_bounaries(__FUNCTION__, dest)) return NULL;
+	uint32_t dest_size_original = dest->p_size;
+
+	if(dest->_a_size >= soft_alloc_size && dest->i_pos < soft_alloc_size && dest->p_size < soft_alloc_size) {
+		dest->p_size = soft_alloc_size;
+		return dest;
+	}
+
+	uint32_t target_soft_alloc_size = soft_alloc_size;
+
+	if(soft_alloc_size * 2 < __ATSC3_UTILS_BLOCK_PREALLOC_RESIZE_DOUBLE_LIMIT__) {
+		target_soft_alloc_size = soft_alloc_size * 2;
+	}
+
+	block_Resize(dest, target_soft_alloc_size);
+	dest->p_size = dest_size_original;
+
+	return dest;
+}
 
 //return will be NULL if realloc failed, but src will still be valid
 //grow or shrink a block,
