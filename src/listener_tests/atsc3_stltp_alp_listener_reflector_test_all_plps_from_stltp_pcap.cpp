d9d896e2dc80073862e72030509b31511169c8eb
Justman, Jason <jjustman@ngbp.org>
2022-10-20 19:47:19 -0700

    adding stltp reflector from pcap

--- src/listener_tests/atsc3_stltp_alp_listener_reflector_test_all_plps_from_stltp_pcap.cpp
+++ src/listener_tests/atsc3_stltp_alp_listener_reflector_test_all_plps_from_stltp_pcap.cpp
@@ -21,14 +21,26 @@
 #include <netinet/ip.h>
 #include <string.h>
 
-#include "../atsc3_listener_udp.h"
-#include "../atsc3_stltp_parser.h"
-#include "../atsc3_alp_parser.h"
-#include "../atsc3_logging_externs.h"
-#include "../atsc3_stltp_depacketizer.h"
 
-#define PREAMBLE_PACKET_PARSE_AND_LOG true
-#define TIMING_MANAGEMENT_PACKET_PARSE_AND_LOG true
+#include <atsc3_utils.h>
+#include <atsc3_listener_udp.h>
+#include <atsc3_pcap_type.h>
+
+#include <atsc3_ntp_utils.h>
+#include <atsc3_logging_externs.h>
+
+#include <atsc3_stltp_types.h>
+
+#include <phy/virtual/PcapSTLTPVirtualPHY.h>
+
+
+
+#define _ATSC3_STLTP_ALP_LISTENER_REFLECTOR_TEST_ALL_PLPS_FROM_STLTP_PCAP_ERROR(...)   printf("%s:%d:ERROR:",__FILE__,__LINE__);_ATSC3_UTILS_PRINTLN(__VA_ARGS__);
+#define _ATSC3_STLTP_ALP_LISTENER_REFLECTOR_TEST_ALL_PLPS_FROM_STLTP_PCAP_WARN(...)    printf("%s:%d:WARN:",__FILE__,__LINE__); _ATSC3_UTILS_PRINTLN(__VA_ARGS__);
+#define _ATSC3_STLTP_ALP_LISTENER_REFLECTOR_TEST_ALL_PLPS_FROM_STLTP_PCAP_INFO(...)    printf("%s:%d:INFO:",__FILE__,__LINE__); _ATSC3_UTILS_PRINTLN(__VA_ARGS__);
+#define _ATSC3_STLTP_ALP_LISTENER_REFLECTOR_TEST_ALL_PLPS_FROM_STLTP_PCAP_DEBUG(...)   printf("%s:%d:DEBUG:",__FILE__,__LINE__);_ATSC3_UTILS_PRINTLN(__VA_ARGS__);
+#define _ATSC3_STLTP_ALP_LISTENER_REFLECTOR_TEST_ALL_PLPS_FROM_STLTP_PCAP_TRACE(...)   printf("%s:%d:TRACE:",__FILE__,__LINE__);_ATSC3_UTILS_PRINTLN(__VA_ARGS__);
+
 
 int PACKET_COUNTER = 0;
 
@@ -40,6 +52,64 @@ void process_packet(u_char *user, const struct pcap_pkthdr *pkthdr, const u_char
 	atsc3_stltp_depacketizer_from_pcap_frame(user, pkthdr, packet, atsc3_stltp_depacketizer_context);
 }
 
+
+int process_stltp_pcap_file_optional_dst_ip_dst_port(char* filename, pcap_t* descrInject, char* stltp_dst_ip, char* stltp_dst_port) {
+
+    atsc3_pcap_replay_context_t* atsc3_pcap_replay_context_volitale = NULL;
+    atsc3_stltp_depacketizer_context_t* atsc3_stltp_depacketizer_context = NULL;
+
+    PcapSTLTPVirtualPHY* pcapSTLTPVirtualPHY = new PcapSTLTPVirtualPHY();
+
+    //pcapSTLTPVirtualPHY->setRxUdpPacketProcessCallback(phy_rx_udp_packet_process_callback);
+    atsc3_stltp_depacketizer_context = pcapSTLTPVirtualPHY->get_atsc3_stltp_depacketizer_context();
+
+    atsc3_stltp_depacketizer_context_set_all_plps(atsc3_stltp_depacketizer_context);
+    atsc3_stltp_depacketizer_context->atsc3_stltp_baseband_alp_packet_collection_callback_with_pcap_device_reference = atsc3_reflect_alp_packet_collection;
+    atsc3_stltp_depacketizer_context->atsc3_baseband_alp_output_pcap_device_reference = descrInject;
+
+    if(stltp_dst_ip && stltp_dst_port) {
+        pcapSTLTPVirtualPHY->atsc3_pcap_stltp_listen_ip_port_plp(stltp_dst_ip, stltp_dst_port, ATSC3_STLTP_DEPACKETIZER_ALL_PLPS_VALUE);
+    }
+    
+    pcapSTLTPVirtualPHY->atsc3_pcap_replay_open_file(filename);
+
+    pcapSTLTPVirtualPHY->atsc3_pcap_thread_run();
+
+    double pcap_thread_run_start_time = gt();
+    sleep(1);
+    while(pcapSTLTPVirtualPHY->is_pcap_replay_running()) {
+        usleep(1000000);
+        atsc3_pcap_replay_context_volitale = pcapSTLTPVirtualPHY->get_pcap_replay_context_status_volatile();
+        //not mutexed, but shouldn't be disposed until we invoke atsc3_pcap_thread_stop
+        if(atsc3_pcap_replay_context_volitale) {
+            _ATSC3_STLTP_ALP_LISTENER_REFLECTOR_TEST_ALL_PLPS_FROM_STLTP_PCAP_DEBUG("pcap_file_pos: %d, pcap_file_len: %d",
+                    atsc3_pcap_replay_context_volitale->pcap_file_pos,
+                    atsc3_pcap_replay_context_volitale->pcap_file_len);
+        }
+    }
+    double pcap_thread_run_end_time = gt();
+    atsc3_pcap_replay_context_volitale = pcapSTLTPVirtualPHY->get_pcap_replay_context_status_volatile();
+
+    double first_packet_ts_s = (atsc3_pcap_replay_context_volitale->first_wallclock_timeval.tv_sec + (atsc3_pcap_replay_context_volitale->first_wallclock_timeval.tv_usec / 1000000.0));
+    double last_packet_ts_s = (atsc3_pcap_replay_context_volitale->last_wallclock_timeval.tv_sec + (atsc3_pcap_replay_context_volitale->last_wallclock_timeval.tv_usec / 1000000.0));
+
+    _ATSC3_STLTP_ALP_LISTENER_REFLECTOR_TEST_ALL_PLPS_FROM_STLTP_PCAP_INFO("...completed, start time: %0.3f, end time: %0.3f, duration: %0.3f, first_packet_ts_sec: %0.3f, last_packet_ts_sec: %0.3f, delta: %0.3f",
+            pcap_thread_run_start_time,
+            pcap_thread_run_end_time,
+            pcap_thread_run_end_time - pcap_thread_run_start_time,
+            first_packet_ts_s,
+            last_packet_ts_s,
+            (last_packet_ts_s - first_packet_ts_s)
+            );
+
+    pcapSTLTPVirtualPHY->atsc3_pcap_thread_stop();
+
+    delete pcapSTLTPVirtualPHY;
+
+    return 0;
+}
+
+
 int main(int argc,char **argv) {
     _IP_UDP_RTP_PARSER_DEBUG_ENABLED = 0;
     _IP_UDP_RTP_PARSER_TRACE_ENABLED = 0;
@@ -48,21 +118,21 @@ int main(int argc,char **argv) {
 	
 	//jjustman-2020-12-31 - extra debugging
 	_STLTP_PARSER_INFO_ENABLED  = 1;
-	_STLTP_PARSER_DUMP_ENABLED  = 1;
-	_STLTP_PARSER_DEBUG_ENABLED = 1;
-	_STLTP_PARSER_TRACE_ENABLED = 1;
+	_STLTP_PARSER_DUMP_ENABLED  = 0;
+	_STLTP_PARSER_DEBUG_ENABLED = 0;
+	_STLTP_PARSER_TRACE_ENABLED = 0;
 
-	_STLTP_TYPES_DEBUG_ENABLED = 1;
-	_STLTP_TYPES_TRACE_ENABLED = 1;
+    _STLTP_TYPES_DEBUG_ENABLED = 0;
+    _STLTP_TYPES_TRACE_ENABLED = 0;
     
-    char *dev;
-    char *devInject;
-    char *filter_dst_ip = NULL;
-    char *filter_dst_port = NULL;
-    char *filter_stltp_plp_id = NULL;
 
-    int dst_port_filter_int;
-    int dst_ip_port_filter_int;
+    char* PCAP_FILENAME = "";
+    
+    char* stltp_dst_ip = NULL;
+    char* stltp_dst_port = NULL;
+
+
+    char *devInject;
 	
     char errbuf[PCAP_ERRBUF_SIZE];
     char errbufInject[PCAP_ERRBUF_SIZE];
@@ -82,75 +152,41 @@ int main(int argc,char **argv) {
 
     atsc3_stltp_depacketizer_context = atsc3_stltp_depacketizer_context_new();
 
-    if(argc < 5) {
+    if(argc < 3) {
         println("%s - an atsc3 stltp udp mulitcast reflector ", argv[0]);
         println("---");
-        println("args: devListen ip port devInject");
-        println(" devListen : device to listen for stltp udp multicast");
-        println(" ip        : ip address for stltp");
-        println(" port      : port for single stltp");
-        println(" devInject : device to inject for ALP IP reflection");
+        println("args: stltp_pcap_file_name devInject (stltp_ip_address) (stltp_port)");
+        println(" stltp_pcap_file_name : STLTP pcap file to replay");
+        println(" devInject            : eth device name for stltp demuxed emission");
+        println("");
+        println(" (stltp_ip_address)    : optional, override auto-detect for stltp ip address from pcap file");
+        println(" (stltp_port)          : optional, override auto-detect for stltp port address from pcap file");
 
         exit(1);
-    } else {
-        dev = argv[1];
-        filter_dst_ip = argv[2];
-        filter_dst_port = argv[3];
-        devInject = argv[4];
-        
-        //parse ip
-        dst_ip_addr_filter = (uint32_t*)calloc(1, sizeof(uint32_t));
-        char* pch = strtok (filter_dst_ip,".");
-        int offset = 24;
-        while (pch != NULL && offset>=0) {
-            uint8_t octet = atoi(pch);
-            *dst_ip_addr_filter |= octet << offset;
-            offset-=8;
-            pch = strtok (NULL, ".");
-        }
-        
-        //parse port
-        
-        dst_port_filter_int = atoi(filter_dst_port);
-        dst_ip_port_filter = (uint16_t*)calloc(1, sizeof(uint16_t));
-        *dst_ip_port_filter |= dst_port_filter_int & 0xFFFF;
-        
-        atsc3_stltp_depacketizer_context->destination_flow_filter.dst_ip_addr = *dst_ip_addr_filter;
-        atsc3_stltp_depacketizer_context->destination_flow_filter.dst_port = *dst_ip_port_filter;
     }
     
-    println("%s -an atsc3 stltp udp mulitcast reflector , listening on dev: %s, reflecting: %s, all plps", argv[0], dev, devInject);
-    
-    pcap_lookupnet(dev, &netp, &maskp, errbuf);
-    descr = pcap_open_live(dev, MAX_PCAP_LEN, 1, 1, errbuf);
-    
-    if(descr == NULL) {
-        printf("pcap_open_live(): %s",errbuf);
-        exit(1);
+    //listen to all flows
+    if(argc >= 3) {
+        PCAP_FILENAME = argv[1];
+        devInject = argv[2];
+
     }
     
-    char filter[] = "udp";
-    if(pcap_compile(descr,&fp, filter,0,netp) == -1) {
-        fprintf(stderr,"Error calling pcap_compile");
-        exit(1);
+    if(argc >= 4) {
+        stltp_dst_ip = argv[3];
+        stltp_dst_port = argv[4];
     }
     
-    if(pcap_setfilter(descr,&fp) == -1) {
-        fprintf(stderr,"Error setting filter");
-        exit(1);
-    }
-   
-    //inject
+    
+    println("%s -an atsc3 stltp udp mulitcast reflector, pcap file: %s, devInject: %s, stltp_dst_ip: %s, stltp_dst_port: %s",
+            argv[0], PCAP_FILENAME, devInject, (stltp_dst_ip ? stltp_dst_ip : "auto"), (stltp_dst_port ? stltp_dst_port : "auto"));
+    
+    //inject dev i/f
     pcap_lookupnet(devInject, &netpInject, &maskpInject, errbufInject);
     pcap_t* descrInject = pcap_open_live(devInject, MAX_PCAP_LEN, 1, 1, errbufInject);
     
-
-    atsc3_stltp_depacketizer_context_set_all_plps(atsc3_stltp_depacketizer_context);
-    atsc3_stltp_depacketizer_context->atsc3_stltp_baseband_alp_packet_collection_callback_with_pcap_device_reference = atsc3_reflect_alp_packet_collection;
-    atsc3_stltp_depacketizer_context->atsc3_baseband_alp_output_pcap_device_reference = descrInject;
-
-    pcap_loop(descr, -1, process_packet, NULL);
-    
+    process_stltp_pcap_file_optional_dst_ip_dst_port(PCAP_FILENAME, descrInject, stltp_dst_ip, stltp_dst_port);
+        
     return 0;
 }
 
